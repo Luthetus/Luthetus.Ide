a7b583696413e89e92f4d60d42ac9832bcd52f47
Hunter Freeman <luthetus@gmail.com>
2023-06-11 13:26:30 -0400

    ITextEditorBackgroundTaskQueue

--- Source/Host/Luthetus.Ide.Photino/Program.cs
+++ Source/Host/Luthetus.Ide.Photino/Program.cs
@@ -5,6 +5,7 @@ using Luthetus.Common.RazorLib.BackgroundTaskCase.Usage;
 using Luthetus.Ide.ClassLib.CompilerServices.HostedServiceCase;
 using Luthetus.Ide.ClassLib.FileSystem.HostedServiceCase;
 using Luthetus.Ide.RazorLib;
+using Luthetus.TextEditor.RazorLib.HostedServiceCase;
 using Microsoft.Extensions.DependencyInjection;
 using Photino.Blazor;
 
@@ -28,6 +29,7 @@ namespace Luthetus.Ide.Photino
             // is not working for the Photino Blazor app.
             // So manual starting of the service is done.
             appBuilder.Services.AddSingleton<CommonQueuedHostedService>();
+            appBuilder.Services.AddSingleton<TextEditorQueuedHostedService>();
             appBuilder.Services.AddSingleton<FileSystemQueuedHostedService>();
             appBuilder.Services.AddSingleton<CompilerServiceQueuedHostedService>();
 
@@ -39,12 +41,14 @@ namespace Luthetus.Ide.Photino
             var backgroundTasksCancellationTokenSource = new CancellationTokenSource();
 
             var commonQueuedHostedService = app.Services.GetRequiredService<CommonQueuedHostedService>();
+            var textEditorQueuedHostedService = app.Services.GetRequiredService<TextEditorQueuedHostedService>();
             var fileSystemQueuedHostedService = app.Services.GetRequiredService<FileSystemQueuedHostedService>();
             var compilerServiceQueuedHostedService = app.Services.GetRequiredService<CompilerServiceQueuedHostedService>();
 
             var cancellationToken = backgroundTasksCancellationTokenSource.Token;
 
             _ = Task.Run(async () => await commonQueuedHostedService.StartAsync(cancellationToken));
+            _ = Task.Run(async () => await textEditorQueuedHostedService.StartAsync(cancellationToken));
             _ = Task.Run(async () => await fileSystemQueuedHostedService.StartAsync(cancellationToken));
             _ = Task.Run(async () => await compilerServiceQueuedHostedService.StartAsync(cancellationToken));
 
