808e7ca58f3c31b5343b07d7dc6130928cd19055
Luthetus <luthetus@gmail.com>
2023-08-23 16:00:16 -0400

    Trying to run the IDE in browser with as much functionality as I can bring

---
+++ Source/Lib/Luthetus.Ide.ClassLib/HostedServiceCase/FileSystem/FileSystemBackgroundTaskQueueSingleThreaded.cs
@@ -0,0 +1,23 @@
+﻿using Luthetus.Common.RazorLib.BackgroundTaskCase.BaseTypes;
+using Luthetus.Ide.ClassLib.HostedServiceCase.FileSystem;
+
+namespace Luthetus.Ide.ClassLib.HostedServiceCase.Terminal;
+
+public class FileSystemBackgroundTaskQueueSingleThreaded : IFileSystemBackgroundTaskQueue
+{
+    public void QueueBackgroundWorkItem(
+        IBackgroundTask backgroundTask)
+    {
+        _ = Task.Run(async () =>
+        {
+            await backgroundTask
+                .InvokeWorkItem(CancellationToken.None);
+        });
+    }
+
+    public Task<IBackgroundTask?> DequeueAsync(
+        CancellationToken cancellationToken)
+    {
+        return Task.FromResult(default(IBackgroundTask?));
+    }
+}
