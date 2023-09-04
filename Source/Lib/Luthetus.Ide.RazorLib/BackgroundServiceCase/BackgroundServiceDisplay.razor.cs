c863272df068c0f44b641265a7c536eb92f0537e
Luthetus <luthetus@gmail.com>
2023-09-04 11:40:54 -0400

    Some UI for BackgroundServicesDisplay.razor

--- Source/Lib/Luthetus.Ide.RazorLib/BackgroundServiceCase/BackgroundServiceDisplay.razor.cs
+++ Source/Lib/Luthetus.Ide.RazorLib/BackgroundServiceCase/BackgroundServiceDisplay.razor.cs
@@ -3,8 +3,26 @@ using Microsoft.AspNetCore.Components;
 
 namespace Luthetus.Ide.RazorLib.BackgroundServiceCase;
 
-public partial class BackgroundServiceDisplay : ComponentBase
+public partial class BackgroundServiceDisplay : ComponentBase, IDisposable
 {
     [Parameter, EditorRequired]
     public IBackgroundTaskService BackgroundTaskQueue { get; set; } = null!;
+    [Parameter, EditorRequired]
+    public string DisplayName { get; set; } = null!;
+
+    protected override void OnInitialized()
+    {
+        BackgroundTaskQueue.ExecutingBackgroundTaskChanged += BackgroundTaskQueue_ExecutingBackgroundTaskChanged;
+        base.OnInitialized();
+    }
+
+    private async void BackgroundTaskQueue_ExecutingBackgroundTaskChanged()
+    {
+        await InvokeAsync(StateHasChanged);
+    }
+
+    public void Dispose()
+    {
+        BackgroundTaskQueue.ExecutingBackgroundTaskChanged -= BackgroundTaskQueue_ExecutingBackgroundTaskChanged;
+    }
 }
