6a764b8a22d6be649854e17acc9f280ecb61d2a3
Luthetus <luthetus@gmail.com>
2023-10-04 11:05:06 -0400

    If ContextRecord is not displayed because it isn't the active Panel Tab then
    Attempting to focus the not-displayed ContextRecord will first make it the active tab

---
+++ Source/Lib/Luthetus.Ide.RazorLib/Commands/ICommandFactory.cs
@@ -0,0 +1,14 @@
+﻿using Luthetus.Common.RazorLib.Commands.Models;
+using Luthetus.Common.RazorLib.Contexts.Models;
+
+namespace Luthetus.Ide.RazorLib.Commands;
+
+public interface ICommandFactory
+{
+    public void Initialize();
+
+    public CommandNoType ConstructFocusContextElementCommand(
+        ContextRecord contextRecord,
+        string displayName,
+        string internalIdentifier);
+}
