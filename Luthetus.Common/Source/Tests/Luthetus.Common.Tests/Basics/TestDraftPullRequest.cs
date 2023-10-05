55e1d10f693e70898589f5a79700e8859a11e765
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2023-10-04 20:40:18 -0400

    Comment in code

--- Luthetus.Common/Source/Tests/Luthetus.Common.Tests/Basics/TestDraftPullRequest.cs
+++ Luthetus.Common/Source/Tests/Luthetus.Common.Tests/Basics/TestDraftPullRequest.cs
@@ -4,9 +4,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Luthetus.Common.Tests.Basics
+namespace Luthetus.Common.Tests.Basics;
+
+public class TestDraftPullRequest
 {
-    internal class TestDraftPullRequest
+    /*
+     * I want to list out the 'topmost' directories within Luthetus.Common.RazorLib
+     * as this might help me plan out testing all the functionality.
+     *
+     * BackgroundTasks
+     * Clipboards
+     * Commands
+     * ComponentRenderers
+     * ComponentRunners
+     * Contexts
+     * CustomEvents
+     * Dialogs
+     * Dimensions
+     * Drags
+     * Dropdowns
+     * FileSystems
+     * Htmls
+     * Icons
+     * Installations
+     * JavaScriptObjects
+     * Keyboards
+     * Keymaps
+     * Keys
+     * Menus
+     * Namespaces
+     * Notifications
+     * Options
+     * OutOfBoundClicks
+     * Panels
+     * Reactives
+     * RenderStates
+     * Resizes
+     * ShouldRenderBoundaries
+     * StateHasChangedBoundaries
+     * Storages
+     * Tabs
+     * Themes
+     * TreeViews
+     * WatchWindows
+     * 
+     * I suppose I'll start by going down the list and make 1 test case foreach
+     * toplevel directory.
+     * 
+     * Then I'll have seen the entire scope of Luthetus.Common.RazorLib, and add
+     * more tests with that knowledge.
+     */
+
+    [Fact]
+    public void Aaa()
     {
+
     }
 }
