c5aa43be3958268813702dca6481d28d6b02109e
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2023-10-30 19:35:24 -0400

    Add Button: ShowCalculationOnClick()

---
+++ Luthetus.TextEditor/Source/Lib/Luthetus.TextEditor.RazorLib/Diffs/Displays/DiffDetailsDisplay.razor.cs
@@ -0,0 +1,11 @@
+using Luthetus.Common.RazorLib.Keys.Models;
+using Luthetus.TextEditor.RazorLib.Diffs.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Luthetus.TextEditor.RazorLib.Diffs.Displays;
+
+public partial class DiffDetailsDisplay : ComponentBase
+{
+    [Parameter, EditorRequired]
+    public Key<TextEditorDiffModel> DiffModelKey { get; set; } = Key<TextEditorDiffModel>.Empty;
+}
