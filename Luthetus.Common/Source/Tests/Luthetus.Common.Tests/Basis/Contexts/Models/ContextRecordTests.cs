2a60853d3784c8733ca4d406a75126c4e50b0385
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2023-11-08 14:35:34 -0500

    42 unit tests passing

--- Luthetus.Common/Source/Tests/Luthetus.Common.Tests/Basis/Contexts/Models/ContextRecordTests.cs
+++ Luthetus.Common/Source/Tests/Luthetus.Common.Tests/Basis/Contexts/Models/ContextRecordTests.cs
@@ -1,4 +1,6 @@
 using Luthetus.Common.RazorLib.Contexts.Models;
+using Luthetus.Common.RazorLib.Keymaps.Models;
+using Luthetus.Common.RazorLib.Keys.Models;
 
 namespace Luthetus.Common.Tests.Basis.Contexts.Models;
 
@@ -8,11 +10,27 @@ namespace Luthetus.Common.Tests.Basis.Contexts.Models;
 public class ContextRecordTests
 {
     /// <summary>
+    /// <see cref="ContextRecord(Key{ContextRecord}, string, string, Keymap)"/>
+    /// <br/>----<br/>
     /// <see cref="ContextRecord.ContextElementId"/>
     /// </summary>
     [Fact]
-    public void ContextElementId()
+    public void Constructor()
     {
-        throw new NotImplementedException();
+        var key = Key<ContextRecord>.NewKey();
+        var displayNameFriendly = "Header";
+        var contextNameInternal = "header";
+        
+        var contextRecord = new ContextRecord(
+            key,
+            displayNameFriendly,
+            contextNameInternal,
+            Keymap.Empty);
+
+        Assert.Equal(key, contextRecord.ContextKey);
+        Assert.Equal(displayNameFriendly, contextRecord.DisplayNameFriendly);
+        Assert.Equal(contextNameInternal, contextRecord.ContextNameInternal);
+        Assert.Equal(Keymap.Empty, contextRecord.Keymap);
+        Assert.Equal($"luth_ide_context-{key.Guid}", contextRecord.ContextElementId);
     }
 }
