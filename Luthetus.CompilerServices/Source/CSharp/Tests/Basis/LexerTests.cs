b28daee580650438dca5984161dd0e2e5f0ec561
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2023-11-27 17:35:57 -0500

    Unimplemented Lexer tests

---
+++ Luthetus.CompilerServices/Source/CSharp/Tests/Basis/LexerTests.cs
@@ -0,0 +1,244 @@
+namespace Luthetus.CompilerServices.Lang.CSharp.Tests.Basis;
+
+public class LexerTests
+{
+	[Fact]
+	public void LEX_ArraySyntaxToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_AssociatedNameToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_AssociatedValueToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_BadToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_BangToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_CloseAngleBracketToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_CloseAssociatedGroupToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_CloseBraceToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_CloseParenthesisToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_CloseSquareBracketToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_ColonToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_CommaToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_CommentMultiLineToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_CommentSingleLineToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_DollarSignToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_EndOfFileToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_EqualsEqualsToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_EqualsToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_IdentifierToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_KeywordContextualToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_KeywordToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_LibraryReferenceToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_MemberAccessToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_MinusMinusToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_MinusToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_NumericLiteralToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_OpenAngleBracketToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_OpenAssociatedGroupToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_OpenBraceToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_OpenParenthesisToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_OpenSquareBracketToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_PlusPlusToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_PlusToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_PreprocessorDirectiveToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_QuestionMarkQuestionMarkToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_QuestionMarkToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_StarToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_StatementDelimiterToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_StringLiteralToken()
+	{
+		throw new NotImplementedException();
+	}
+
+	[Fact]
+	public void LEX_TriviaToken()
+	{
+		throw new NotImplementedException();
+	}
+}
