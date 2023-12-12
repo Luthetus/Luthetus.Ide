0b5cb920693050e5384a4afe99cd547e2782137a
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2023-12-12 17:28:13 -0500

    Update TextEditorModel.ConstructorsTests.cs

--- Luthetus.TextEditor/Source/Tests/Luthetus.TextEditor.Tests/Basis/TextEditors/Models/TextEditorModels/TextEditorModel.ConstructorsTests.cs
+++ Luthetus.TextEditor/Source/Tests/Luthetus.TextEditor.Tests/Basis/TextEditors/Models/TextEditorModels/TextEditorModel.ConstructorsTests.cs
@@ -1,5 +1,9 @@
 ﻿using Xunit;
 using Luthetus.TextEditor.RazorLib.TextEditors.Models.TextEditorModels;
+using Luthetus.TextEditor.RazorLib.Lexes.Models;
+using Luthetus.TextEditor.RazorLib.Decorations.Models;
+using Luthetus.TextEditor.RazorLib.CompilerServices;
+using Luthetus.TextEditor.RazorLib.Keymaps.Models;
 
 namespace Luthetus.TextEditor.Tests.Basis.TextEditors.Models.TextEditorModels;
 
@@ -9,12 +13,35 @@ namespace Luthetus.TextEditor.Tests.Basis.TextEditors.Models.TextEditorModels;
 public class TextEditorModelConstructorsTests
 {
 	/// <summary>
-	/// <see cref="TextEditorModel(RazorLib.Lexes.Models.ResourceUri, DateTime, string, string, RazorLib.Decorations.Models.IDecorationMapper?, RazorLib.CompilerServices.ICompilerService?, Common.RazorLib.Keymaps.Models.Keymap?)"/>
+	/// <see cref="TextEditorModel(ResourceUri, DateTime, string, string, RazorLib.Decorations.Models.IDecorationMapper?, RazorLib.CompilerServices.ICompilerService?, Common.RazorLib.Keymaps.Models.Keymap?)"/>
 	/// </summary>
 	[Fact]
 	public void Constructor_New()
 	{
-		throw new NotImplementedException();
+		var resourceUri = new ResourceUri("/unitTests.txt");
+		var resourceLastWriteTime = DateTime.UtcNow;
+		var fileExtension = ".txt";
+		var content = "Hello World!";
+		var decorationMapper = new TextEditorDecorationMapperDefault();
+		var compilerService = new TextEditorDefaultCompilerService();
+		var keymap = TextEditorKeymapFacts.DefaultKeymap;
+
+        var model = new TextEditorModel(
+            resourceUri,
+            resourceLastWriteTime,
+            fileExtension,
+            content,
+            decorationMapper,
+            compilerService,
+            keymap);
+
+		Assert.Equal(resourceUri, model.ResourceUri);
+		Assert.Equal(resourceLastWriteTime, model.ResourceLastWriteTime);
+		Assert.Equal(fileExtension, model.FileExtension);
+		Assert.Equal(content, model.GetAllText());
+		Assert.Equal(decorationMapper, model.DecorationMapper);
+		Assert.Equal(compilerService, model.CompilerService);
+		Assert.Equal(keymap, model.TextEditorKeymap);
 	}
 
 	/// <summary>
@@ -23,6 +50,54 @@ public class TextEditorModelConstructorsTests
 	[Fact]
 	public void Constructor_Clone()
 	{
-		throw new NotImplementedException();
+        var originalModel = new TextEditorModel(
+            new ResourceUri("/unitTests.txt"),
+            DateTime.UtcNow,
+            ".txt",
+            "Hello World!",
+            new TextEditorDecorationMapperDefault(),
+            new TextEditorDefaultCompilerService(),
+            TextEditorKeymapFacts.DefaultKeymap);
+
+		var cloneModel = new TextEditorModel(
+			originalModel.ContentBag,
+			originalModel.EditBlocksBag,
+            originalModel.RowEndingPositionsBag,
+            originalModel.RowEndingKindCountsBag,
+            originalModel.PresentationModelsBag,
+            originalModel.TabKeyPositionsBag,
+            originalModel.OnlyRowEndingKind,
+			originalModel.UsingRowEndingKind,
+			originalModel.ResourceUri,
+			originalModel.ResourceLastWriteTime,
+			originalModel.FileExtension,
+			originalModel.DecorationMapper,
+			originalModel.CompilerService,
+			originalModel.TextEditorSaveFileHelper,
+			originalModel.EditBlockIndex,
+			originalModel.MostCharactersOnASingleRowTuple,
+			originalModel.RenderStateKey,
+			originalModel.TextEditorKeymap,
+			originalModel.TextEditorOptions);
+
+        Assert.Equal(originalModel.ContentBag, cloneModel.ContentBag);
+		Assert.Equal(originalModel.EditBlocksBag, cloneModel.EditBlocksBag);
+        Assert.Equal(originalModel.RowEndingPositionsBag, cloneModel.RowEndingPositionsBag);
+        Assert.Equal(originalModel.RowEndingKindCountsBag, cloneModel.RowEndingKindCountsBag);
+        Assert.Equal(originalModel.PresentationModelsBag, cloneModel.PresentationModelsBag);
+        Assert.Equal(originalModel.TabKeyPositionsBag, cloneModel.TabKeyPositionsBag);
+        Assert.Equal(originalModel.OnlyRowEndingKind, cloneModel.OnlyRowEndingKind);
+		Assert.Equal(originalModel.UsingRowEndingKind, cloneModel.UsingRowEndingKind);
+		Assert.Equal(originalModel.ResourceUri, cloneModel.ResourceUri);
+		Assert.Equal(originalModel.ResourceLastWriteTime, cloneModel.ResourceLastWriteTime);
+		Assert.Equal(originalModel.FileExtension, cloneModel.FileExtension);
+		Assert.Equal(originalModel.DecorationMapper, cloneModel.DecorationMapper);
+		Assert.Equal(originalModel.CompilerService, cloneModel.CompilerService);
+		Assert.Equal(originalModel.TextEditorSaveFileHelper, cloneModel.TextEditorSaveFileHelper);
+		Assert.Equal(originalModel.EditBlockIndex, cloneModel.EditBlockIndex);
+		Assert.Equal(originalModel.MostCharactersOnASingleRowTuple, cloneModel.MostCharactersOnASingleRowTuple);
+		Assert.Equal(originalModel.RenderStateKey, cloneModel.RenderStateKey);
+		Assert.Equal(originalModel.TextEditorKeymap, cloneModel.TextEditorKeymap);
+		Assert.Equal(originalModel.TextEditorOptions, cloneModel.TextEditorOptions);
 	}
 }
