9f9428cb95341730b4f27bdcbcf9594b845d2d4c
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2023-12-24 21:26:45 -0500

    ConstraintNodeTests: progress

--- Luthetus.TextEditor/Source/Tests/Luthetus.TextEditor.Tests/Basis/CompilerServices/Syntax/SyntaxNodes/ConstraintNodeTests.cs
+++ Luthetus.TextEditor/Source/Tests/Luthetus.TextEditor.Tests/Basis/CompilerServices/Syntax/SyntaxNodes/ConstraintNodeTests.cs
@@ -8,46 +8,51 @@ namespace Luthetus.TextEditor.Tests.Basis.CompilerServices.Syntax.SyntaxNodes;
 /// </summary>
 public class ConstraintNodeTests
 {
-	/// <summary>
-	/// <see cref="ConstraintNode(System.Collections.Immutable.ImmutableArray{RazorLib.CompilerServices.Syntax.ISyntaxToken})"/>
-	/// </summary>
-	[Fact]
+    /// <summary>
+    /// <see cref="ConstraintNode(System.Collections.Immutable.ImmutableArray{RazorLib.CompilerServices.Syntax.ISyntaxToken})"/>
+    /// </summary>
+    [Fact]
 	public void Constructor()
-	{
-		throw new NotImplementedException();
-	}
+    {
+        var constraintText = @"where T : notnull";
+        var sourceText = $@"public abstract class TreeViewWithType<T> : TreeViewNoType {constraintText}
+{{
+}}";
 
-	/// <summary>
-	/// <see cref="ConstraintNode.InnerTokens"/>
-	/// </summary>
-	[Fact]
+        throw new NotImplementedException(_ = sourceText);
+    }
+
+    /// <summary>
+    /// <see cref="ConstraintNode.InnerTokens"/>
+    /// </summary>
+    [Fact]
 	public void InnerTokens()
 	{
 		throw new NotImplementedException();
 	}
 
-	/// <summary>
-	/// <see cref="ConstraintNode.ChildBag"/>
-	/// </summary>
-	[Fact]
+    /// <summary>
+    /// <see cref="ConstraintNode.ChildBag"/>
+    /// </summary>
+    [Fact]
 	public void ChildBag()
 	{
 		throw new NotImplementedException();
 	}
 
-	/// <summary>
-	/// <see cref="ConstraintNode.IsFabricated"/>
-	/// </summary>
-	[Fact]
+    /// <summary>
+    /// <see cref="ConstraintNode.IsFabricated"/>
+    /// </summary>
+    [Fact]
 	public void IsFabricated()
 	{
 		throw new NotImplementedException();
 	}
 
-	/// <summary>
-	/// <see cref="ConstraintNode.SyntaxKind"/>
-	/// </summary>
-	[Fact]
+    /// <summary>
+    /// <see cref="ConstraintNode.SyntaxKind"/>
+    /// </summary>
+    [Fact]
 	public void SyntaxKind()
 	{
 		throw new NotImplementedException();
