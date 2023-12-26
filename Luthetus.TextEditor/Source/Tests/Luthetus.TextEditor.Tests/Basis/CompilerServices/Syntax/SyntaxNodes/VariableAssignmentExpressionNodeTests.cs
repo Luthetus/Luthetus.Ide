f0d5796ba5e519303286e4ba477433027499ce43
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2023-12-25 20:32:01 -0500

    Tests: VariableAssignmentExpressionNodeTests

--- Luthetus.TextEditor/Source/Tests/Luthetus.TextEditor.Tests/Basis/CompilerServices/Syntax/SyntaxNodes/VariableAssignmentExpressionNodeTests.cs
+++ Luthetus.TextEditor/Source/Tests/Luthetus.TextEditor.Tests/Basis/CompilerServices/Syntax/SyntaxNodes/VariableAssignmentExpressionNodeTests.cs
@@ -1,5 +1,8 @@
 ﻿using Xunit;
 using Luthetus.TextEditor.RazorLib.CompilerServices.Syntax.SyntaxNodes;
+using Luthetus.TextEditor.RazorLib.CompilerServices.Syntax.SyntaxTokens;
+using Luthetus.TextEditor.RazorLib.CompilerServices.Syntax.SyntaxNodes.Expression;
+using Luthetus.TextEditor.RazorLib.Lexes.Models;
 
 namespace Luthetus.TextEditor.Tests.Basis.CompilerServices.Syntax.SyntaxNodes;
 
@@ -8,66 +11,93 @@ namespace Luthetus.TextEditor.Tests.Basis.CompilerServices.Syntax.SyntaxNodes;
 /// </summary>
 public class VariableAssignmentExpressionNodeTests
 {
-	/// <summary>
-	/// <see cref="VariableAssignmentExpressionNode(RazorLib.CompilerServices.Syntax.SyntaxTokens.IdentifierToken, RazorLib.CompilerServices.Syntax.SyntaxTokens.EqualsToken, RazorLib.CompilerServices.Syntax.SyntaxNodes.Expression.IExpressionNode)"/>
-	/// </summary>
-	[Fact]
+    /// <summary>
+    /// <see cref="VariableAssignmentExpressionNode(IdentifierToken, EqualsToken, IExpressionNode)"/>
+    /// <br/>----<br/>
+    /// <see cref="VariableAssignmentExpressionNode.VariableIdentifierToken"/>
+    /// <see cref="VariableAssignmentExpressionNode.EqualsToken"/>
+    /// <see cref="VariableAssignmentExpressionNode.ExpressionNode"/>
+    /// <see cref="VariableAssignmentExpressionNode.ChildBag"/>
+    /// <see cref="VariableAssignmentExpressionNode.IsFabricated"/>
+    /// <see cref="VariableAssignmentExpressionNode.SyntaxKind"/>
+    /// </summary>
+    [Fact]
 	public void Constructor()
 	{
-		throw new NotImplementedException();
-	}
+        var sourceText = "x = 3";
 
-	/// <summary>
-	/// <see cref="VariableAssignmentExpressionNode.VariableIdentifierToken"/>
-	/// </summary>
-	[Fact]
-	public void VariableIdentifierToken()
-	{
-		throw new NotImplementedException();
-	}
+        IdentifierToken variableIdentifierToken;
+        {
+            var variableIdentifierText = "x";
+            int indexOfVariableIdentifierText = sourceText.IndexOf(variableIdentifierText);
+            
+            variableIdentifierToken = new IdentifierToken(new TextEditorTextSpan(
+                indexOfVariableIdentifierText,
+                indexOfVariableIdentifierText + variableIdentifierText.Length,
+                0,
+                new ResourceUri("/unitTesting.txt"),
+                sourceText));
+        }
 
-	/// <summary>
-	/// <see cref="VariableAssignmentExpressionNode.EqualsToken"/>
-	/// </summary>
-	[Fact]
-	public void EqualsToken()
-	{
-		throw new NotImplementedException();
-	}
+        EqualsToken equalsToken;
+        {
+            var equalsText = "=";
+            int indexOfEqualsText = sourceText.IndexOf(equalsText);
 
-	/// <summary>
-	/// <see cref="VariableAssignmentExpressionNode.ExpressionNode"/>
-	/// </summary>
-	[Fact]
-	public void ExpressionNode()
-	{
-		throw new NotImplementedException();
-	}
+            equalsToken = new EqualsToken(new TextEditorTextSpan(
+                indexOfEqualsText,
+                indexOfEqualsText + equalsText.Length,
+                0,
+                new ResourceUri("/unitTesting.txt"),
+                sourceText));
+        }
 
-	/// <summary>
-	/// <see cref="VariableAssignmentExpressionNode.ChildBag"/>
-	/// </summary>
-	[Fact]
-	public void ChildBag()
-	{
-		throw new NotImplementedException();
-	}
+        IExpressionNode expressionNode;
+        {
+            var numericLiteralText = "3";
+            int indexOfNumericLiteralText = sourceText.IndexOf(numericLiteralText);
+            var numericLiteralToken = new NumericLiteralToken(new TextEditorTextSpan(
+                indexOfNumericLiteralText,
+                indexOfNumericLiteralText + numericLiteralText.Length,
+                0,
+                new ResourceUri("/unitTesting.txt"),
+                sourceText));
 
-	/// <summary>
-	/// <see cref="VariableAssignmentExpressionNode.IsFabricated"/>
-	/// </summary>
-	[Fact]
-	public void IsFabricated()
-	{
-		throw new NotImplementedException();
-	}
+            TypeClauseNode intTypeClauseNode;
+            {
+                var intTypeIdentifier = new KeywordToken(
+                    TextEditorTextSpan.FabricateTextSpan("int"),
+                    RazorLib.CompilerServices.Syntax.SyntaxKind.IntTokenKeyword);
 
-	/// <summary>
-	/// <see cref="VariableAssignmentExpressionNode.SyntaxKind"/>
-	/// </summary>
-	[Fact]
-	public void SyntaxKind()
-	{
-		throw new NotImplementedException();
+                intTypeClauseNode = new TypeClauseNode(
+                    intTypeIdentifier,
+                    typeof(int),
+                    null);
+            }
+
+            expressionNode = new LiteralExpressionNode(
+                numericLiteralToken,
+                intTypeClauseNode);
+        }
+
+        var variableAssignmentExpressionNode = new VariableAssignmentExpressionNode(
+            variableIdentifierToken,
+            equalsToken,
+            expressionNode);
+
+        Assert.Equal(variableIdentifierToken, variableAssignmentExpressionNode.VariableIdentifierToken);
+        Assert.Equal(equalsToken, variableAssignmentExpressionNode.EqualsToken);
+        Assert.Equal(expressionNode, variableAssignmentExpressionNode.ExpressionNode);
+
+        Assert.Equal(3, variableAssignmentExpressionNode.ChildBag.Length);
+        Assert.Equal(variableIdentifierToken, variableAssignmentExpressionNode.ChildBag[0]);
+        Assert.Equal(equalsToken, variableAssignmentExpressionNode.ChildBag[1]);
+        Assert.Equal(expressionNode, variableAssignmentExpressionNode.ChildBag[2]);
+
+        Assert.False(variableAssignmentExpressionNode.IsFabricated);
+
+        Assert.Equal(
+            RazorLib.CompilerServices.Syntax.SyntaxKind.VariableAssignmentExpressionNode,
+            variableAssignmentExpressionNode.SyntaxKind);
 	}
 }
