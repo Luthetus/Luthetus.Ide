df0fc5f40afc5fed3789b2cfecdeaa1fd822ea3a
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2023-12-29 14:25:14 -0500

    TextEditorCursorModifierTests

---
+++ Luthetus.TextEditor/Source/Tests/Luthetus.TextEditor.Tests/Basis/Cursors/Models/TextEditorCursorModifierTests.cs
@@ -0,0 +1,109 @@
+﻿using Xunit;
+using Luthetus.TextEditor.RazorLib.Cursors.Models;
+
+namespace Luthetus.TextEditor.Tests.Basis.Cursors.Models;
+
+/// <summary>
+/// <see cref="TextEditorCursorModifier"/>
+/// </summary>
+public class TextEditorCursorModifierTests
+{
+    /// <summary>
+    /// <see cref="TextEditorCursorModifier(TextEditorCursor)"/>
+    /// </summary>
+    [Fact]
+    public void Constructor()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// <see cref="TextEditorCursorModifier.RowIndex"/>
+    /// </summary>
+    [Fact]
+    public void RowIndex()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// <see cref="TextEditorCursorModifier.ColumnIndex"/>
+    /// </summary>
+    [Fact]
+    public void ColumnIndex()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// <see cref="TextEditorCursorModifier.PreferredColumnIndex"/>
+    /// </summary>
+    [Fact]
+    public void PreferredColumnIndex()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// <see cref="TextEditorCursorModifier.IsPrimaryCursor"/>
+    /// </summary>
+    [Fact]
+    public void IsPrimaryCursor()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// <see cref="TextEditorCursorModifier.SelectionAnchorPositionIndex"/>
+    /// </summary>
+    [Fact]
+    public void SelectionAnchorPositionIndex()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// <see cref="TextEditorCursorModifier.SelectionEndingPositionIndex"/>
+    /// </summary>
+    [Fact]
+    public void SelectionEndingPositionIndex()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// <see cref="TextEditorCursorModifier.Key"/>
+    /// </summary>
+    [Fact]
+    public void Key()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// <see cref="TextEditorCursorModifier.ShouldRevealCursor"/>
+    /// </summary>
+    [Fact]
+    public void ShouldRevealCursor()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// <see cref="TextEditorCursorModifier.IsIntersecting"/>
+    /// </summary>
+    [Fact]
+    public void IsIntersecting()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// <see cref="TextEditorCursorModifier.ToCursor()"/>
+    /// </summary>
+    [Fact]
+    public void ToCursor()
+    {
+        throw new NotImplementedException();
+    }
+}
