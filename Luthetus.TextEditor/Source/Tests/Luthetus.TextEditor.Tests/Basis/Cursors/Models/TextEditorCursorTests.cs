1b4629131000e57bf6e766eda6cedff555f72e34
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2023-12-30 12:18:02 -0500

    TextEditorCursorTests skeleton

--- Luthetus.TextEditor/Source/Tests/Luthetus.TextEditor.Tests/Basis/Cursors/Models/TextEditorCursorTests.cs
+++ Luthetus.TextEditor/Source/Tests/Luthetus.TextEditor.Tests/Basis/Cursors/Models/TextEditorCursorTests.cs
@@ -1,5 +1,5 @@
-using Xunit;
 using Luthetus.TextEditor.RazorLib.Cursors.Models;
+using Xunit;
 
 namespace Luthetus.TextEditor.Tests.Basis.Cursors.Models;
 
@@ -8,93 +8,69 @@ namespace Luthetus.TextEditor.Tests.Basis.Cursors.Models;
 /// </summary>
 public class TextEditorCursorTests
 {
-	/// <summary>
-	/// <see cref="TextEditorCursor(bool)"/>
-	/// </summary>
-	[Fact]
-	public void Constructor_A()
-	{
-		throw new NotImplementedException();
-	}
+    [Fact]
+    public void ConstructorA()
+    {
+        throw new NotImplementedException();
+    }
 
-	/// <summary>
-	/// <see cref="TextEditorCursor(ValueTuple{int, int}, bool)"/>
-	/// </summary>
-	[Fact]
-	public void Constructor_B()
-	{
-		throw new NotImplementedException();
-	}
+    [Fact]
+    public void ConstructorB()
+    {
+        throw new NotImplementedException();
+    }
 
-	/// <summary>
-	/// <see cref="TextEditorCursor(ImmutableTextEditorCursor, bool)"/>
-	/// </summary>
-	[Fact]
-	public void Constructor_C()
-	{
-		throw new NotImplementedException();
-	}
+    [Fact]
+    public void Empty()
+    {
+        throw new NotImplementedException();
+    }
 
-	/// <summary>
-	/// <see cref="TextEditorCursor.IndexCoordinates"/>
-	/// </summary>
-	[Fact]
-	public void IndexCoordinates()
-	{
-		throw new NotImplementedException();
-	}
+    [Fact]
+    public void RowIndex()
+    {
+        throw new NotImplementedException();
+    }
 
-	/// <summary>
-	/// <see cref="TextEditorCursor.PreferredColumnIndex"/>
-	/// </summary>
-	[Fact]
-	public void PreferredColumnIndex()
-	{
-		throw new NotImplementedException();
-	}
+    [Fact]
+    public void ColumnIndex()
+    {
+        throw new NotImplementedException();
+    }
 
-	/// <summary>
-	/// <see cref="TextEditorCursor.Selection"/>
-	/// </summary>
-	[Fact]
-	public void Selection()
-	{
-		throw new NotImplementedException();
-	}
+    [Fact]
+    public void PreferredColumnIndex()
+    {
+        throw new NotImplementedException();
+    }
 
-	/// <summary>
-	/// <see cref="TextEditorCursor.ShouldRevealCursor"/>
-	/// </summary>
-	[Fact]
-	public void ShouldRevealCursor()
-	{
-		throw new NotImplementedException();
-	}
+    [Fact]
+    public void IsPrimaryCursor()
+    {
+        throw new NotImplementedException();
+    }
 
-	/// <summary>
-	/// <see cref="TextEditorCursor.IsPrimaryCursor"/>
-	/// </summary>
-	[Fact]
-	public void IsPrimaryCursor()
-	{
-		throw new NotImplementedException();
-	}
+    [Fact]
+    public void Selection()
+    {
+        throw new NotImplementedException();
+    }
 
-	/// <summary>
-	/// <see cref="TextEditorCursor.IsIntersecting"/>
-	/// </summary>
-	[Fact]
-	public void IsIntersecting()
-	{
-		throw new NotImplementedException();
-	}
+    [Fact]
+    public void Key()
+    {
+        throw new NotImplementedException();
+    }
 
-	/// <summary>
-	/// <see cref="TextEditorCursor.MoveCursor(Microsoft.AspNetCore.Components.Web.KeyboardEventArgs, TextEditorCursor, RazorLib.TextEditors.Models.TextEditorModels.TextEditorModel)"/>
-	/// </summary>
-	[Fact]
-	public void MoveCursor()
-	{
-		throw new NotImplementedException();
-	}
+    [Fact]
+    public void ShouldRevealCursor()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public void IsIntersecting()
+    {
+        throw new NotImplementedException();
+    }
 }
