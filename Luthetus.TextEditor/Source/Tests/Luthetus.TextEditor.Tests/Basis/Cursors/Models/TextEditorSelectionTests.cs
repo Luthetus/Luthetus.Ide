d240bba969cf9493dfc05411c031daa71d8624a1
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2023-12-30 13:27:49 -0500

    Tests: TextEditorSelectionTests

--- Luthetus.TextEditor/Source/Tests/Luthetus.TextEditor.Tests/Basis/Cursors/Models/TextEditorSelectionTests.cs
+++ Luthetus.TextEditor/Source/Tests/Luthetus.TextEditor.Tests/Basis/Cursors/Models/TextEditorSelectionTests.cs
@@ -8,21 +8,50 @@ namespace Luthetus.TextEditor.Tests.Basis.Cursors.Models;
 /// </summary>
 public class TextEditorSelectionTests
 {
-	/// <summary>
-	/// <see cref="TextEditorSelection.AnchorPositionIndex"/>
-	/// </summary>
-	[Fact]
-	public void AnchorPositionIndex()
+    /// <summary>
+    /// <see cref="TextEditorSelection(int?, int)"/>
+	/// <br/>----<br/>
+    /// <see cref="TextEditorSelection.AnchorPositionIndex"/>
+    /// <see cref="TextEditorSelection.EndingPositionIndex"/>
+    /// </summary>
+    [Fact]
+	public void Constructor()
 	{
-		throw new NotImplementedException();
-	}
+        var anchorPositionIndex = 1;
+        var endingPositionIndex = 3;
+        var selection = new TextEditorSelection(anchorPositionIndex, endingPositionIndex);
 
-	/// <summary>
-	/// <see cref="TextEditorSelection.EndingPositionIndex"/>
-	/// </summary>
-	[Fact]
-	public void EndingPositionIndex()
-	{
-		throw new NotImplementedException();
+        Assert.Equal(anchorPositionIndex, selection.AnchorPositionIndex);
+        Assert.Equal(endingPositionIndex, selection.EndingPositionIndex);
+
+        // Use 'with' to change AnchorPositionIndex.
+        {
+            var outAnchorPositionIndex = selection.AnchorPositionIndex + 1;
+
+            var outSelection = selection with
+            {
+                AnchorPositionIndex = outAnchorPositionIndex
+            };
+
+            // Assert the change
+            Assert.Equal(outAnchorPositionIndex, outSelection.AnchorPositionIndex);
+            // Assert that the old EndingPositionIndex was carried over unchanged.
+            Assert.Equal(endingPositionIndex, outSelection.EndingPositionIndex);
+        }
+
+        // Use 'with' to change EndingPositionIndex.
+        {
+            var outEndingPositionIndex = selection.EndingPositionIndex + 1;
+
+            var outSelection = selection with
+            {
+                EndingPositionIndex = outEndingPositionIndex
+            };
+
+            // Assert that the old AnchorPositionIndex was carried over unchanged.
+            Assert.Equal(anchorPositionIndex, outSelection.AnchorPositionIndex);
+            // Assert the change
+            Assert.Equal(outEndingPositionIndex, outSelection.EndingPositionIndex);
+        }
 	}
 }
