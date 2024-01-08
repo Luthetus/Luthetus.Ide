61106c8f40daadf392686c2ec62921c54beb7daf
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-01-08 16:50:43 -0500

    Tests: StringWalkerTests progress

--- Luthetus.TextEditor/Source/Tests/Luthetus.TextEditor.Tests/Basis/CompilerServices/StringWalkerTests.cs
+++ Luthetus.TextEditor/Source/Tests/Luthetus.TextEditor.Tests/Basis/CompilerServices/StringWalkerTests.cs
@@ -1,5 +1,7 @@
 ﻿using Xunit;
 using Luthetus.TextEditor.RazorLib.CompilerServices;
+using Luthetus.TextEditor.RazorLib.Lexes.Models;
+using Luthetus.TextEditor.Tests.Basis.TextEditors.Models.TextEditorServices;
 
 namespace Luthetus.TextEditor.Tests.Basis.CompilerServices;
 
@@ -8,107 +10,116 @@ namespace Luthetus.TextEditor.Tests.Basis.CompilerServices;
 /// </summary>
 public class StringWalkerTests
 {
-	/// <summary>
-	/// <see cref="StringWalker(RazorLib.Lexes.Models.ResourceUri, string)"/>
-	/// </summary>
-	[Fact]
+    /// <summary>
+    /// <see cref="StringWalker(ResourceUri, string)"/>
+	/// <br/>----<br/>
+    /// <see cref="StringWalker.PositionIndex"/>
+    /// <see cref="StringWalker.ResourceUri"/>
+    /// <see cref="StringWalker.SourceText"/>
+    /// <see cref="StringWalker.CurrentCharacter"/>
+    /// <see cref="StringWalker.NextCharacter"/>
+    /// <see cref="StringWalker.RemainingText"/>
+    /// <see cref="StringWalker.IsEof"/>
+	/// <see cref="StringWalker.ReadCharacter()"/>
+	/// <see cref="StringWalker.PeekCharacter(int)"/>
+	/// <see cref="StringWalker.BacktrackCharacter()"/>
+	/// <see cref="StringWalker.ReadRange(int)"/>
+    /// </summary>
+    [Fact]
 	public void Constructor()
 	{
-		throw new NotImplementedException();
+        var resourceUri = new ResourceUri("/unitTesting.txt");
+		var sourceText = TestConstants.SOURCE_TEXT;
+
+        var stringWalker = new StringWalker(resourceUri, sourceText);
+
+		// Assert initial values of Properties
+		{
+			Assert.Equal(0, stringWalker.PositionIndex);
+			Assert.Equal(resourceUri, stringWalker.ResourceUri);
+			Assert.Equal(sourceText, stringWalker.SourceText);
+			Assert.Equal('H', stringWalker.CurrentCharacter);
+			Assert.Equal('e', stringWalker.NextCharacter);
+			Assert.Equal("Hello World!\n7 Pillows\n \n,abc123", stringWalker.RemainingText);
+			Assert.False(stringWalker.IsEof);
+		}
+
+		// Read a character
+		{
+			Assert.Equal('H', stringWalker.ReadCharacter());
+
+			// Assert values of Properties after having read a character
+            Assert.Equal(1, stringWalker.PositionIndex);
+            Assert.Equal(resourceUri, stringWalker.ResourceUri);
+            Assert.Equal(sourceText, stringWalker.SourceText);
+            Assert.Equal('e', stringWalker.CurrentCharacter);
+            Assert.Equal('l', stringWalker.NextCharacter);
+            Assert.Equal("ello World!\n7 Pillows\n \n,abc123", stringWalker.RemainingText);
+            Assert.False(stringWalker.IsEof);
+		}
+
+		// Peek a character
+		{
+			// Peek current character
+            Assert.Equal('e', stringWalker.PeekCharacter(0));
+
+            // Peek next character
+            Assert.Equal('l', stringWalker.PeekCharacter(1));
+
+            // Peek the character two indices higher
+            Assert.Equal('l', stringWalker.PeekCharacter(2));
+
+            // Peek the character three indices higher
+            Assert.Equal('o', stringWalker.PeekCharacter(3));
+			
+			// Assert values of Properties after having peeked a character
+			// These values should not have changed since the peek method was used.
+			Assert.Equal(1, stringWalker.PositionIndex);
+			Assert.Equal(resourceUri, stringWalker.ResourceUri);
+			Assert.Equal(sourceText, stringWalker.SourceText);
+			Assert.Equal('e', stringWalker.CurrentCharacter);
+			Assert.Equal('l', stringWalker.NextCharacter);
+			Assert.Equal("ello World!\n7 Pillows\n \n,abc123", stringWalker.RemainingText);
+			Assert.False(stringWalker.IsEof);
+        }
+
+		// Backtrack a character
+		{
+			Assert.Equal('H', stringWalker.BacktrackCharacter());
+
+			// Assert values of Properties after having backtracked a character
+			// These values should be equal to the initial state.
+			Assert.Equal(0, stringWalker.PositionIndex);
+			Assert.Equal(resourceUri, stringWalker.ResourceUri);
+			Assert.Equal(sourceText, stringWalker.SourceText);
+			Assert.Equal('H', stringWalker.CurrentCharacter);
+			Assert.Equal('e', stringWalker.NextCharacter);
+			Assert.Equal("Hello World!\n7 Pillows\n \n,abc123", stringWalker.RemainingText);
+			Assert.False(stringWalker.IsEof);
+		}
+
+		// Read a range of characters
+		{
+			var goalString = "Hello";
+            var charsToRead = goalString.Length;
+            Assert.Equal(goalString, stringWalker.ReadRange(charsToRead));
+
+            // Assert values of Properties after having read a range of characters
+            Assert.Equal(goalString.Length, stringWalker.PositionIndex);
+            Assert.Equal(resourceUri, stringWalker.ResourceUri);
+            Assert.Equal(sourceText, stringWalker.SourceText);
+            Assert.Equal(' ', stringWalker.CurrentCharacter);
+            Assert.Equal('W', stringWalker.NextCharacter);
+            Assert.Equal(" World!\n7 Pillows\n \n,abc123", stringWalker.RemainingText);
+            Assert.False(stringWalker.IsEof);
+        }
+
+        
+
+        throw new NotImplementedException();
 	}
 
 	/// <summary>
-	/// <see cref="StringWalker.PositionIndex"/>
-	/// </summary>
-	[Fact]
-	public void PositionIndex()
-	{
-		throw new NotImplementedException();
-	}
-
-	/// <summary>
-	/// <see cref="StringWalker.ResourceUri"/>
-	/// </summary>
-	[Fact]
-	public void ResourceUri()
-	{
-		throw new NotImplementedException();
-	}
-
-	/// <summary>
-	/// <see cref="StringWalker.SourceText"/>
-	/// </summary>
-	[Fact]
-	public void SourceText()
-	{
-		throw new NotImplementedException();
-	}
-
-	/// <summary>
-	/// <see cref="StringWalker.CurrentCharacter"/>
-	/// </summary>
-	[Fact]
-	public void CurrentCharacter()
-	{
-		throw new NotImplementedException();
-	}
-
-	/// <summary>
-	/// <see cref="StringWalker.NextCharacter"/>
-	/// </summary>
-	[Fact]
-	public void NextCharacter()
-	{
-		throw new NotImplementedException();
-	}
-
-	/// <summary>
-	/// <see cref="StringWalker.RemainingText"/>
-	/// </summary>
-	[Fact]
-	public void RemainingText()
-	{
-		throw new NotImplementedException();
-	}
-
-	/// <summary>
-	/// <see cref="StringWalker.IsEof"/>
-	/// </summary>
-	[Fact]
-	public void IsEof()
-	{
-		throw new NotImplementedException();
-	}
-
-	/// <summary>
-	/// <see cref="StringWalker.ReadCharacter()"/>
-	/// </summary>
-	[Fact]
-	public void ReadCharacter()
-	{
-		throw new NotImplementedException();
-	}
-
-	/// <summary>
-	/// <see cref="StringWalker.PeekCharacter(int)"/>
-	/// </summary>
-	[Fact]
-	public void PeekCharacter()
-	{
-		throw new NotImplementedException();
-	}
-
-	/// <summary>
-	/// <see cref="StringWalker.BacktrackCharacter()"/>
-	/// </summary>
-	[Fact]
-	public void BacktrackCharacter()
-	{
-		throw new NotImplementedException();
-	}
-
-	/// <summary>
-	/// <see cref="StringWalker.ReadRange(int)"/>
 	/// </summary>
 	[Fact]
 	public void ReadRange()
