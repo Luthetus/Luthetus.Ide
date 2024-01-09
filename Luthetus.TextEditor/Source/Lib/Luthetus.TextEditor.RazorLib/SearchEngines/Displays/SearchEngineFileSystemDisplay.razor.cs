ee98f1d8c408462a4147ba293c218c60f64749cc
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-01-09 18:44:05 -0500

    An initial search attempt worked

--- Luthetus.TextEditor/Source/Lib/Luthetus.TextEditor.RazorLib/SearchEngines/Displays/SearchEngineFileSystemDisplay.razor.cs
+++ Luthetus.TextEditor/Source/Lib/Luthetus.TextEditor.RazorLib/SearchEngines/Displays/SearchEngineFileSystemDisplay.razor.cs
@@ -3,8 +3,24 @@ using Luthetus.TextEditor.RazorLib.SearchEngines.Models;
 
 namespace Luthetus.TextEditor.RazorLib.SearchEngines.Displays;
 
-public partial class SearchEngineFileSystemDisplay : ComponentBase
+public partial class SearchEngineFileSystemDisplay : ComponentBase, IDisposable
 {
 	[Parameter, EditorRequired]
 	public SearchEngineFileSystem SearchEngineFileSystem { get; set; } = null!;
+
+	protected override void OnInitialized()
+	{
+		SearchEngineFileSystem.ProgressOccurred += On_SearchEngineFileSystem_ProgressOccurred;
+		base.OnInitialized();
+	}
+
+	private async void On_SearchEngineFileSystem_ProgressOccurred()
+	{
+		await InvokeAsync(StateHasChanged);
+	}
+
+	public void Dispose()
+	{
+		SearchEngineFileSystem.ProgressOccurred -= On_SearchEngineFileSystem_ProgressOccurred;
+	}
 }
--- Luthetus.TextEditor/Source/Lib/Luthetus.TextEditor.RazorLib/SearchEngines/Models/SearchEngineFileSystem.cs
+++ Luthetus.TextEditor/Source/Lib/Luthetus.TextEditor.RazorLib/SearchEngines/Models/SearchEngineFileSystem.cs
@@ -7,6 +7,8 @@ namespace Luthetus.TextEditor.RazorLib.SearchEngines.Models;
 public class SearchEngineFileSystem : ITextEditorSearchEngine
 {
 	private readonly IFileSystemProvider _fileSystemProvider;
+	
+	private int _runCount;
 
 	public SearchEngineFileSystem(IFileSystemProvider fileSystemProvider)
 	{
@@ -19,11 +21,70 @@ public class SearchEngineFileSystem : ITextEditorSearchEngine
     public Type IconComponentRendererType { get; } = typeof(IconCopy);
     public string DisplayName { get; } = "FileSystem";
 	public string StartingDirectoryPath = "C:\\Users\\hunte\\Repos\\Luthetus.Ide_Fork\\";
-	public List<string> ResultBag = new List<string>();
+	public List<string> FilePathBag = new List<string>();
+	public event Action? ProgressOccurred;
+	public bool IsSearching;
 
     public async Task SearchAsync(string searchQuery, CancellationToken cancellationToken = default)
     {
-		ResultBag.Clear();
-		ResultBag.AddRange(await _fileSystemProvider.Directory.GetDirectoriesAsync(StartingDirectoryPath));
+		if (_runCount != 0)
+			return;
+
+		_runCount++;
+
+		_ =  Task.Run(async () => 
+		{
+			FilePathBag.Clear();
+			IsSearching = true;
+			ProgressOccurred?.Invoke();
+			
+			await RecursiveSearchAsync(StartingDirectoryPath, searchQuery, cancellationToken);
+			IsSearching = false;
+			ProgressOccurred?.Invoke();
+			_runCount--;
+		});
     }
+
+	private async Task RecursiveSearchAsync(string directoryPath, string searchQuery, CancellationToken cancellationToken = default)
+	{
+		// Considering the use a breadth first algorithm
+
+		// Search Files
+		{
+			var childFileBag = await _fileSystemProvider.Directory.GetFilesAsync(
+				directoryPath);
+
+			foreach (var childFile in childFileBag)
+			{
+				await PerformSearchFileAsync(childFile, searchQuery, cancellationToken);
+			}
+		}
+
+		// Update UI with progress
+		{
+			ProgressOccurred?.Invoke();
+		}
+
+		// Recurse into subdirectories
+		{
+			var subdirectoryBag = await _fileSystemProvider.Directory.GetDirectoriesAsync(
+				directoryPath);
+
+			foreach (var subdirectory in subdirectoryBag)
+			{
+				if (subdirectory.Contains(".git")  || subdirectory.Contains("bin") || subdirectory.Contains("obj"))
+					continue;
+
+				await RecursiveSearchAsync(subdirectory, searchQuery, cancellationToken);
+			}
+		}
+	}
+
+	private async Task PerformSearchFileAsync(string filePath, string searchQuery, CancellationToken cancellationToken = default)
+	{
+		var contents = await _fileSystemProvider.File.ReadAllTextAsync(filePath);
+
+		if (contents.Contains(searchQuery))
+			FilePathBag.Add(filePath);
+	}
 }
