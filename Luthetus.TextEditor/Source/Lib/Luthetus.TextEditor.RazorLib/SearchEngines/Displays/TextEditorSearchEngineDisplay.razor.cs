2419859944ce4f0b58d28e9d7f91f99d484bf942
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-01-10 15:33:10 -0500

    Tab component progress

--- Luthetus.TextEditor/Source/Lib/Luthetus.TextEditor.RazorLib/SearchEngines/Displays/TextEditorSearchEngineDisplay.razor.cs
+++ Luthetus.TextEditor/Source/Lib/Luthetus.TextEditor.RazorLib/SearchEngines/Displays/TextEditorSearchEngineDisplay.razor.cs
@@ -1,7 +1,12 @@
 using Fluxor;
 using Fluxor.Blazor.Web.Components;
+using Luthetus.Common.RazorLib.Keys.Models;
+using Luthetus.Common.RazorLib.Tabs.Models;
+using Luthetus.Common.RazorLib.Tabs.States;
+using Luthetus.TextEditor.RazorLib.SearchEngines.Models;
 using Luthetus.TextEditor.RazorLib.SearchEngines.States;
 using Microsoft.AspNetCore.Components;
+using System.Collections.Immutable;
 
 namespace Luthetus.TextEditor.RazorLib.SearchEngines.Displays;
 
@@ -10,7 +15,11 @@ public partial class TextEditorSearchEngineDisplay : FluxorComponent
     [Inject]
     private IState<TextEditorSearchEngineState> TextEditorSearchEngineStateWrap { get; set; } = null!;
     [Inject]
+    private IStateSelection<TabState, TabGroup?> TabStateGroupSelection { get; set; } = null!;
+    [Inject]
     private IDispatcher Dispatcher { get; set; } = null!;
+
+    private static readonly Key<TabGroup> SelectedSearchEngineTabGroupKey = new(Guid.Parse("92ec8823-79b3-4be3-99c5-1c68d713e685"));
 
     private CancellationTokenSource _doSearchCancellationTokenSource = new();
     private bool _isSearching;
@@ -24,6 +33,61 @@ public partial class TextEditorSearchEngineDisplay : FluxorComponent
             if (value is not null)
                 Dispatcher.Dispatch(new TextEditorSearchEngineState.SetSearchQueryAction(value));
         }
+    }
+
+    protected override void OnInitialized()
+    {
+        TabStateGroupSelection.Select(tabState => tabState.TabGroupList.FirstOrDefault(
+            group => group.Key == SelectedSearchEngineTabGroupKey));
+
+        base.OnInitialized();
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            var tabGroup = TabStateGroupSelection.Value;
+
+            if (tabGroup is null)
+            {
+                var searchEngineState = TextEditorSearchEngineStateWrap.Value;
+
+                tabGroup = new TabGroup(
+                    args =>
+                    {
+                        var searchEngineList = searchEngineState.SearchEngineList;
+                        var tabEntryList = new List<TabEntryNoType>();
+
+                        foreach (var searchEngine in searchEngineList)
+                        {
+                            var tabEntryWithType = new TabEntryWithType<ITextEditorSearchEngine>(
+                                searchEngine,
+                                tabEntryNoType => ((TabEntryWithType<ITextEditorSearchEngine>)tabEntryNoType)
+                                    .Item.DisplayName,
+                                tabEntryNoType => Dispatcher.Dispatch(new TabState.SetActiveTabEntryKeyAction(
+                                    SelectedSearchEngineTabGroupKey,
+                                    tabEntryNoType.TabEntryKey)));
+
+                            tabEntryList.Add(tabEntryWithType);
+                        }
+
+                        return Task.FromResult(
+                            new TabGroupLoadTabEntriesOutput(tabEntryList.ToImmutableList()));
+                    },
+                    SelectedSearchEngineTabGroupKey);
+
+                Dispatcher.Dispatch(new TabState.RegisterTabGroupAction(tabGroup));
+
+                var entries = await tabGroup.LoadEntryListAsync();
+
+                Dispatcher.Dispatch(new TabState.SetTabEntryListAction(
+                    SelectedSearchEngineTabGroupKey,
+                    entries.OutTabEntries));
+            }
+        }
+
+        await base.OnAfterRenderAsync(firstRender);
     }
 
     private async Task DoSearchOnClickAsync(TextEditorSearchEngineState searchEngineState)
