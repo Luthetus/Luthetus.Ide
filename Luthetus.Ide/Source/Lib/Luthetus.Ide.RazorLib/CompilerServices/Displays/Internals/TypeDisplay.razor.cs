b15356ddc3499c766c33ba01911c0277e11cd4cd
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-01-28 16:14:06 -0500

    Some data for TypeDisplay.razor

--- Luthetus.Ide/Source/Lib/Luthetus.Ide.RazorLib/CompilerServices/Displays/Internals/TypeDisplay.razor.cs
+++ Luthetus.Ide/Source/Lib/Luthetus.Ide.RazorLib/CompilerServices/Displays/Internals/TypeDisplay.razor.cs
@@ -1,3 +1,4 @@
+using Luthetus.CompilerServices.Lang.CSharp.CompilerServiceCase;
 using Luthetus.TextEditor.RazorLib.CompilerServices.Syntax;
 using Microsoft.AspNetCore.Components;
 
@@ -5,6 +6,11 @@ namespace Luthetus.Ide.RazorLib.CompilerServices.Displays.Internals;
 
 public partial class TypeDisplay : ComponentBase
 {
+    [CascadingParameter, EditorRequired]
+    public CSharpCompilerService CSharpCompilerService { get; set; } = null!;
+    [CascadingParameter, EditorRequired]
+    public CSharpResource CSharpResource { get; set; } = null!;
+
     [Parameter, EditorRequired]
     public ISyntaxNode SyntaxNode { get; set; } = null!;
 }
