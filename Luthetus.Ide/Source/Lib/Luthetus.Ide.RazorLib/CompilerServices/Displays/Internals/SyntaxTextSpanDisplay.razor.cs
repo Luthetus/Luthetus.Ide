44272bba195db31ccb3d9f63f0c8c2889d9d9d91
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-01-29 15:08:28 -0500

    EditText

--- Luthetus.Ide/Source/Lib/Luthetus.Ide.RazorLib/CompilerServices/Displays/Internals/SyntaxTextSpanDisplay.razor.cs
+++ Luthetus.Ide/Source/Lib/Luthetus.Ide.RazorLib/CompilerServices/Displays/Internals/SyntaxTextSpanDisplay.razor.cs
@@ -1,15 +1,24 @@
+using Luthetus.Common.RazorLib.Keys.Models;
+using Luthetus.TextEditor.RazorLib.Cursors.Models;
 using Luthetus.TextEditor.RazorLib.Lexes.Models;
+using Luthetus.TextEditor.RazorLib.TextEditors.Models;
+using Luthetus.TextEditor.RazorLib.TextEditors.Models.TextEditorModels;
+using Luthetus.TextEditor.RazorLib.TextEditors.Models.TextEditorServices;
 using Microsoft.AspNetCore.Components;
 
 namespace Luthetus.Ide.RazorLib.CompilerServices.Displays.Internals;
 
 public partial class SyntaxTextSpanDisplay : ComponentBase
 {
+    [Inject]
+    private ITextEditorService TextEditorService { get; set; } = null!;
+
     [Parameter, EditorRequired]
     public TextEditorTextSpan TextSpan { get; set; } = null!;
 
     private (TextEditorTextSpan TextEditorTextSpan, string GetTextResult) _textSpanTuple;
-    private string InputValue { get; set; } = null!;
+
+    private string InputValue { get; set; } = string.Empty;
 
     protected override void OnParametersSet()
     {
@@ -20,5 +29,46 @@ public partial class SyntaxTextSpanDisplay : ComponentBase
         }
 
         base.OnParametersSet();
+    }
+
+    private void CompleteForm()
+    {
+        var localTextSpanTuple = _textSpanTuple;
+        var localInputValue = InputValue;
+
+        TextEditorService.Post(nameof(SyntaxTextSpanDisplay), editContext =>
+        {
+            var modelModifier = editContext.GetModelModifier(
+                localTextSpanTuple.TextEditorTextSpan.ResourceUri);
+
+            if (modelModifier is null)
+                return Task.CompletedTask;
+
+            var rowInfo = modelModifier.GetRowInformationFromPositionIndex(
+                localTextSpanTuple.TextEditorTextSpan.StartingIndexInclusive);
+
+            var columnIndex = localTextSpanTuple.TextEditorTextSpan.StartingIndexInclusive -
+                rowInfo.RowStartPositionIndexInclusive;
+
+            var cursor = new TextEditorCursor(
+                rowInfo.RowIndex,
+                columnIndex,
+                columnIndex,
+                true,
+                new TextEditorSelection(
+                    localTextSpanTuple.TextEditorTextSpan.StartingIndexInclusive,
+                    localTextSpanTuple.TextEditorTextSpan.EndingIndexExclusive));
+
+            var cursorModifierBag = new TextEditorCursorModifierBag(
+                Key<TextEditorViewModel>.Empty,
+                new List<TextEditorCursorModifier> { new(cursor) });
+
+            return TextEditorService.ModelApi.InsertTextUnsafeFactory(
+                    _textSpanTuple.TextEditorTextSpan.ResourceUri,
+                    cursorModifierBag,
+                    localInputValue,
+                    CancellationToken.None)
+                .Invoke(editContext);
+        });
     }
 }
