32500f39d4bd6a9cf91548a45ec60343e7989d90
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-01-29 15:23:51 -0500

    Some safeguards to ensure model is what one thinks it is

--- Luthetus.Ide/Source/Lib/Luthetus.Ide.RazorLib/CompilerServices/Displays/Internals/SyntaxTextSpanDisplay.razor.cs
+++ Luthetus.Ide/Source/Lib/Luthetus.Ide.RazorLib/CompilerServices/Displays/Internals/SyntaxTextSpanDisplay.razor.cs
@@ -1,3 +1,4 @@
+using Luthetus.Common.RazorLib.Keyboards.Models;
 using Luthetus.Common.RazorLib.Keys.Models;
 using Luthetus.TextEditor.RazorLib.Cursors.Models;
 using Luthetus.TextEditor.RazorLib.Lexes.Models;
@@ -5,6 +6,8 @@ using Luthetus.TextEditor.RazorLib.TextEditors.Models;
 using Luthetus.TextEditor.RazorLib.TextEditors.Models.TextEditorModels;
 using Luthetus.TextEditor.RazorLib.TextEditors.Models.TextEditorServices;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System.Collections.Immutable;
 
 namespace Luthetus.Ide.RazorLib.CompilerServices.Displays.Internals;
 
@@ -31,18 +34,33 @@ public partial class SyntaxTextSpanDisplay : ComponentBase
         base.OnParametersSet();
     }
 
+    private void HandleOnKeyDown(KeyboardEventArgs keyboardEventArgs)
+    {
+        if (keyboardEventArgs.Code == KeyboardKeyFacts.WhitespaceCodes.ENTER_CODE)
+            CompleteForm();
+    }
+
     private void CompleteForm()
     {
         var localTextSpanTuple = _textSpanTuple;
         var localInputValue = InputValue;
+        var model = TextEditorService.ModelApi.GetOrDefault(localTextSpanTuple.TextEditorTextSpan.ResourceUri);
 
-        TextEditorService.Post(nameof(SyntaxTextSpanDisplay), editContext =>
+        if (model is null)
+            return;
+
+        var modelText = model.GetAllText();
+
+        TextEditorService.Post(nameof(SyntaxTextSpanDisplay), async editContext =>
         {
             var modelModifier = editContext.GetModelModifier(
                 localTextSpanTuple.TextEditorTextSpan.ResourceUri);
 
-            if (modelModifier is null)
-                return Task.CompletedTask;
+            if (modelModifier is null ||
+                modelModifier.GetAllText() != modelText)
+            {
+                return;
+            }
 
             var rowInfo = modelModifier.GetRowInformationFromPositionIndex(
                 localTextSpanTuple.TextEditorTextSpan.StartingIndexInclusive);
@@ -63,12 +81,16 @@ public partial class SyntaxTextSpanDisplay : ComponentBase
                 Key<TextEditorViewModel>.Empty,
                 new List<TextEditorCursorModifier> { new(cursor) });
 
-            return TextEditorService.ModelApi.InsertTextUnsafeFactory(
+            await TextEditorService.ModelApi.InsertTextUnsafeFactory(
                     _textSpanTuple.TextEditorTextSpan.ResourceUri,
                     cursorModifierBag,
                     localInputValue,
                     CancellationToken.None)
                 .Invoke(editContext);
+
+            modelModifier.CompilerService.ResourceWasModified(
+                _textSpanTuple.TextEditorTextSpan.ResourceUri,
+                ImmutableArray<TextEditorTextSpan>.Empty);
         });
     }
 }
