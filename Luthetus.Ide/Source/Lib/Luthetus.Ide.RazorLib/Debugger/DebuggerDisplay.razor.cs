765597a9ebf98633d7cfee3e51f779440cf5aaba
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-02-09 14:13:02 -0500

    Add button to start with debugging (unimplemented)

---
+++ Luthetus.Ide/Source/Lib/Luthetus.Ide.RazorLib/Debugger/DebuggerDisplay.razor.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Luthetus.Ide.RazorLib.Debugger;
+
+public partial class DebuggerDisplay : ComponentBase
+{
+
+}
--- Luthetus.Ide/Source/Lib/Luthetus.Ide.RazorLib/StartupControls/Displays/StartupControlsDisplay.razor.cs
+++ Luthetus.Ide/Source/Lib/Luthetus.Ide.RazorLib/StartupControls/Displays/StartupControlsDisplay.razor.cs
@@ -6,6 +6,10 @@ using Luthetus.Ide.RazorLib.ProgramExecutions.States;
 using Luthetus.Ide.RazorLib.Terminals.States;
 using Luthetus.Ide.RazorLib.Terminals.Models;
 using Microsoft.AspNetCore.Components;
+using Luthetus.Common.RazorLib.Dialogs.Models;
+using Luthetus.Common.RazorLib.Dialogs.States;
+using Luthetus.Common.RazorLib.FileSystems.Displays;
+using Luthetus.Ide.RazorLib.Debugger;
 
 namespace Luthetus.Ide.RazorLib.StartupControls.Displays;
 
@@ -15,6 +19,8 @@ public partial class StartupControlsDisplay : FluxorComponent
     private IState<ProgramExecutionState> ProgramExecutionStateWrap { get; set; } = null!;
     [Inject]
     private IState<TerminalSessionState> TerminalSessionStateWrap { get; set; } = null!;
+    [Inject]
+    private IDispatcher Dispatcher { get; set; } = null!;
 
     private readonly Key<TerminalCommand> _newDotNetSolutionTerminalCommandKey = Key<TerminalCommand>.NewKey();
     private readonly CancellationTokenSource _newDotNetSolutionCancellationTokenSource = new();
@@ -46,5 +52,49 @@ public partial class StartupControlsDisplay : FluxorComponent
         await executionTerminalSession
             .EnqueueCommandAsync(startProgramWithoutDebuggingCommand)
             .ConfigureAwait(false);
+    }
+    
+    private async Task StartProgramWithDebuggingOnClick()
+    {
+        var programExecutionState = ProgramExecutionStateWrap.Value;
+
+        if (programExecutionState.StartupProjectAbsolutePath is null)
+            return;
+
+        var ancestorDirectory = programExecutionState.StartupProjectAbsolutePath.ParentDirectory;
+
+        if (ancestorDirectory is null)
+            return;
+
+        var formattedCommand = DotNetCliCommandFormatter.FormatStartProjectWithoutDebugging(
+            programExecutionState.StartupProjectAbsolutePath);
+
+        var startProgramWithoutDebuggingCommand = new TerminalCommand(
+            _newDotNetSolutionTerminalCommandKey,
+            formattedCommand,
+            ancestorDirectory.Path,
+            _newDotNetSolutionCancellationTokenSource.Token);
+
+        var executionTerminalSession = TerminalSessionStateWrap.Value.TerminalSessionMap[
+            TerminalSessionFacts.EXECUTION_TERMINAL_SESSION_KEY];
+
+        await executionTerminalSession
+            .EnqueueCommandAsync(startProgramWithoutDebuggingCommand)
+            .ConfigureAwait(false);
+
+        // Show debug dialog
+        {
+            var dialogRecord = new DialogRecord(
+                Key<DialogRecord>.NewKey(),
+                "Debug",
+                typeof(DebuggerDisplay),
+                null,
+                null)
+                {
+                    IsResizable = true
+                };
+
+            Dispatcher.Dispatch(new DialogState.RegisterAction(dialogRecord));
+        }
     }
 }
