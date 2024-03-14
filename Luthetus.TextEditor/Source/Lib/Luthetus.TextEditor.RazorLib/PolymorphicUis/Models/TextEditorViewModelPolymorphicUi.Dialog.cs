45f8e8dd0ab653dc6467b512410d4dbfa361b367
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-03-14 11:20:26 -0400

    Add using statements

--- Luthetus.TextEditor/Source/Lib/Luthetus.TextEditor.RazorLib/PolymorphicUis/Models/TextEditorViewModelPolymorphicUi.Dialog.cs
+++ Luthetus.TextEditor/Source/Lib/Luthetus.TextEditor.RazorLib/PolymorphicUis/Models/TextEditorViewModelPolymorphicUi.Dialog.cs
@@ -1,3 +1,19 @@
+using Luthetus.Common.RazorLib.Dialogs.Models;
+using Luthetus.Common.RazorLib.PolymorphicUis.Displays;
+using Luthetus.TextEditor.RazorLib.TextEditors.Displays;
+using Luthetus.TextEditor.RazorLib.TextEditors.Models;
+using Luthetus.TextEditor.RazorLib.TextEditors.Models.TextEditorServices;
+using Luthetus.TextEditor.RazorLib.TextEditors.Models.Internals;
+using Luthetus.TextEditor.RazorLib.Groups.Models;
+using Luthetus.TextEditor.RazorLib.TextEditors.Displays;
+using Luthetus.Common.RazorLib.PolymorphicUis.Models;
+using Luthetus.Common.RazorLib.Dimensions.Models;
+using Luthetus.Common.RazorLib.Keys.Models;
+using Luthetus.Common.RazorLib.JavaScriptObjects.Models;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components.Web;
+using System.Collections.Immutable;
+
 namespace Luthetus.TextEditor.RazorLib.PolymorphicUis.Models;
 
 public partial record TextEditorViewModelPolymorphicUi : IPolymorphicDialog
