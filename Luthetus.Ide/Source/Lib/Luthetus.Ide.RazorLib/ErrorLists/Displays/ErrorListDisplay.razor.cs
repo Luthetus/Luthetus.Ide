ea555101db37d8f825d9e5b010358c6d7b4e38a0
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-03-19 11:55:38 -0400

    Delete dead code in: OutputDisplay.razor/.cs

--- Luthetus.Ide/Source/Lib/Luthetus.Ide.RazorLib/ErrorLists/Displays/ErrorListDisplay.razor.cs
+++ Luthetus.Ide/Source/Lib/Luthetus.Ide.RazorLib/ErrorLists/Displays/ErrorListDisplay.razor.cs
@@ -1,8 +1,21 @@
+using Fluxor;
+using Fluxor.Blazor.Web.Components;
+using Luthetus.Common.RazorLib.Keys.Models;
+using Luthetus.TextEditor.RazorLib.Htmls.Models;
+using Luthetus.TextEditor.RazorLib.TextEditors.Models.TextEditorServices;
+using Luthetus.TextEditor.RazorLib.Lexes.Models;
+using Luthetus.TextEditor.RazorLib.TextEditors.Models;
+using Luthetus.TextEditor.RazorLib.TextEditors.Models.Internals;
+using Luthetus.Ide.RazorLib.Terminals.States;
+using Luthetus.Ide.RazorLib.Terminals.Models;
+using Luthetus.Ide.RazorLib.Outputs.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System.Text;
 
 namespace Luthetus.Ide.RazorLib.ErrorLists.Displays;
 
-public partial class ErrorListDisplay : ComponentBase
+public partial class ErrorListDisplay : FluxorComponent
 {
 	
 }
