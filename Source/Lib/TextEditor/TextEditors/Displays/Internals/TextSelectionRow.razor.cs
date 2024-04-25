17fa714cd2b7305e796ad4de2bbe944e8104a2f7
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-04-25 17:15:48 -0400

    Fix: exception in TextSelectionRow.razor

--- Source/Lib/TextEditor/TextEditors/Displays/Internals/TextSelectionRow.razor.cs
+++ Source/Lib/TextEditor/TextEditors/Displays/Internals/TextSelectionRow.razor.cs
@@ -6,6 +6,7 @@ using Luthetus.TextEditor.RazorLib.TextEditors.Models.Internals;
 using Luthetus.Common.RazorLib.Dimensions.Models;
 using Luthetus.TextEditor.RazorLib.TextEditors.Models;
 using Luthetus.TextEditor.RazorLib.JsRuntimes.Models;
+using Luthetus.TextEditor.RazorLib.Exceptions;
 
 namespace Luthetus.TextEditor.RazorLib.TextEditors.Displays.Internals;
 
@@ -42,132 +43,139 @@ public partial class TextSelectionRow : ComponentBase
 
     private async Task GetTextSelectionStyleCssAsync()
     {
-        int lowerPositionIndexInclusive = LowerPositionIndexInclusive;
-        int upperPositionIndexExclusive = UpperPositionIndexExclusive;
-        int rowIndex = RowIndex;
-
-        if (rowIndex >= RenderBatch.Model!.LineEndList.Count)
-            return;
-
-        bool stateHasChanged = false;
-
-        var line = RenderBatch.Model!.GetLineInformation(rowIndex);
-
-        var selectionStartingColumnIndex = 0;
-        var selectionEndingColumnIndex = line.EndPositionIndexExclusive - 1;
-
-        var fullWidthOfRowIsSelected = true;
-
-        if (lowerPositionIndexInclusive > line.StartPositionIndexInclusive)
+        try
         {
-            selectionStartingColumnIndex = lowerPositionIndexInclusive - line.StartPositionIndexInclusive;
-            fullWidthOfRowIsSelected = false;
-        }
+            int lowerPositionIndexInclusive = LowerPositionIndexInclusive;
+            int upperPositionIndexExclusive = UpperPositionIndexExclusive;
+            int rowIndex = RowIndex;
 
-        if (upperPositionIndexExclusive < line.EndPositionIndexExclusive)
-        {
-            selectionEndingColumnIndex = upperPositionIndexExclusive - line.StartPositionIndexInclusive;
-            fullWidthOfRowIsSelected = false;
-        }
+            if (rowIndex >= RenderBatch.Model!.LineEndList.Count)
+                return;
 
-        var charMeasurements = RenderBatch.ViewModel!.VirtualizationResult.CharAndLineMeasurements;
+            bool stateHasChanged = false;
 
-        var topInPixelsInvariantCulture = (rowIndex * charMeasurements.LineHeight).ToCssValue();
-        var top = $"top: {topInPixelsInvariantCulture}px;";
+            var line = RenderBatch.Model!.GetLineInformation(rowIndex);
 
-        var heightInPixelsInvariantCulture = charMeasurements.LineHeight.ToCssValue();
-        var height = $"height: {heightInPixelsInvariantCulture}px;";
+            var selectionStartingColumnIndex = 0;
+            var selectionEndingColumnIndex = line.EndPositionIndexExclusive - 1;
 
-        // _selectionStartingLeftRelativeToParentInPixels
-        {
-            var selectionStartingCursor = new TextEditorCursor(
-                rowIndex,
-                selectionStartingColumnIndex,
-                true);
+            var fullWidthOfRowIsSelected = true;
 
-            var textOffsettingCursor = RenderBatch.Model!
-                .GetTextOffsettingCursor(selectionStartingCursor)
-                .EscapeHtml();
-
-            var guid = Guid.NewGuid();
-
-            var nextSelectionStartingLeftRelativeToParentInPixels = await JsRuntime.GetLuthetusTextEditorApi()
-                .CalculateProportionalLeftOffset(
-                    ProportionalFontMeasurementsContainerElementId,
-                    $"luth_te_proportional-font-measurement-parent_{RenderBatch.ViewModel!.ViewModelKey.Guid}_selection_{guid}",
-                    $"luth_te_proportional-font-measurement-cursor_{RenderBatch.ViewModel!.ViewModelKey.Guid}_selection_{guid}",
-                    textOffsettingCursor,
-                    true);
-
-            var previousSelectionStartingLeftRelativeToParentInPixels = _selectionStartingLeftRelativeToParentInPixels;
-
-            _selectionStartingLeftRelativeToParentInPixels = nextSelectionStartingLeftRelativeToParentInPixels;
-
-            if ((int)nextSelectionStartingLeftRelativeToParentInPixels !=
-                (int)previousSelectionStartingLeftRelativeToParentInPixels)
+            if (lowerPositionIndexInclusive > line.StartPositionIndexInclusive)
             {
-                stateHasChanged = true;
+                selectionStartingColumnIndex = lowerPositionIndexInclusive - line.StartPositionIndexInclusive;
+                fullWidthOfRowIsSelected = false;
             }
-        }
 
-        var selectionStartInPixelsInvariantCulture = _selectionStartingLeftRelativeToParentInPixels.ToCssValue();
-        var left = $"left: {selectionStartInPixelsInvariantCulture}px;";
+            if (upperPositionIndexExclusive < line.EndPositionIndexExclusive)
+            {
+                selectionEndingColumnIndex = upperPositionIndexExclusive - line.StartPositionIndexInclusive;
+                fullWidthOfRowIsSelected = false;
+            }
 
-        // _selectionWidthInPixels
-        {
-            var selectionEndingCursor = new TextEditorCursor(
-                rowIndex,
-                selectionEndingColumnIndex,
-                true);
+            var charMeasurements = RenderBatch.ViewModel!.VirtualizationResult.CharAndLineMeasurements;
 
-            var textOffsettingCursor = RenderBatch.Model!
-                .GetTextOffsettingCursor(selectionEndingCursor)
-                .EscapeHtml();
+            var topInPixelsInvariantCulture = (rowIndex * charMeasurements.LineHeight).ToCssValue();
+            var top = $"top: {topInPixelsInvariantCulture}px;";
 
-            var guid = Guid.NewGuid();
+            var heightInPixelsInvariantCulture = charMeasurements.LineHeight.ToCssValue();
+            var height = $"height: {heightInPixelsInvariantCulture}px;";
 
-            var selectionEndingLeftRelativeToParentInPixels = await JsRuntime.GetLuthetusTextEditorApi()
-                .CalculateProportionalLeftOffset(
-                    ProportionalFontMeasurementsContainerElementId,
-                    $"luth_te_proportional-font-measurement-parent_{RenderBatch.ViewModel!.ViewModelKey.Guid}_selection_{guid}",
-                    $"luth_te_proportional-font-measurement-cursor_{RenderBatch.ViewModel!.ViewModelKey.Guid}_selection_{guid}",
-                    textOffsettingCursor,
+            // _selectionStartingLeftRelativeToParentInPixels
+            {
+                var selectionStartingCursor = new TextEditorCursor(
+                    rowIndex,
+                    selectionStartingColumnIndex,
                     true);
 
-            var nextSelectionWidthInPixels = selectionEndingLeftRelativeToParentInPixels -
-                _selectionStartingLeftRelativeToParentInPixels;
+                var textOffsettingCursor = RenderBatch.Model!
+                    .GetTextOffsettingCursor(selectionStartingCursor)
+                    .EscapeHtml();
 
-            var previousSelectionWidthInPixels = _selectionWidthInPixels;
+                var guid = Guid.NewGuid();
 
-            _selectionWidthInPixels = nextSelectionWidthInPixels;
+                var nextSelectionStartingLeftRelativeToParentInPixels = await JsRuntime.GetLuthetusTextEditorApi()
+                    .CalculateProportionalLeftOffset(
+                        ProportionalFontMeasurementsContainerElementId,
+                        $"luth_te_proportional-font-measurement-parent_{RenderBatch.ViewModel!.ViewModelKey.Guid}_selection_{guid}",
+                        $"luth_te_proportional-font-measurement-cursor_{RenderBatch.ViewModel!.ViewModelKey.Guid}_selection_{guid}",
+                        textOffsettingCursor,
+                        true);
 
-            if ((int)nextSelectionWidthInPixels != (int)previousSelectionWidthInPixels)
-                stateHasChanged = true;
+                var previousSelectionStartingLeftRelativeToParentInPixels = _selectionStartingLeftRelativeToParentInPixels;
+
+                _selectionStartingLeftRelativeToParentInPixels = nextSelectionStartingLeftRelativeToParentInPixels;
+
+                if ((int)nextSelectionStartingLeftRelativeToParentInPixels !=
+                    (int)previousSelectionStartingLeftRelativeToParentInPixels)
+                {
+                    stateHasChanged = true;
+                }
+            }
+
+            var selectionStartInPixelsInvariantCulture = _selectionStartingLeftRelativeToParentInPixels.ToCssValue();
+            var left = $"left: {selectionStartInPixelsInvariantCulture}px;";
+
+            // _selectionWidthInPixels
+            {
+                var selectionEndingCursor = new TextEditorCursor(
+                    rowIndex,
+                    selectionEndingColumnIndex,
+                    true);
+
+                var textOffsettingCursor = RenderBatch.Model!
+                    .GetTextOffsettingCursor(selectionEndingCursor)
+                    .EscapeHtml();
+
+                var guid = Guid.NewGuid();
+
+                var selectionEndingLeftRelativeToParentInPixels = await JsRuntime.GetLuthetusTextEditorApi()
+                    .CalculateProportionalLeftOffset(
+                        ProportionalFontMeasurementsContainerElementId,
+                        $"luth_te_proportional-font-measurement-parent_{RenderBatch.ViewModel!.ViewModelKey.Guid}_selection_{guid}",
+                        $"luth_te_proportional-font-measurement-cursor_{RenderBatch.ViewModel!.ViewModelKey.Guid}_selection_{guid}",
+                        textOffsettingCursor,
+                        true);
+
+                var nextSelectionWidthInPixels = selectionEndingLeftRelativeToParentInPixels -
+                    _selectionStartingLeftRelativeToParentInPixels;
+
+                var previousSelectionWidthInPixels = _selectionWidthInPixels;
+
+                _selectionWidthInPixels = nextSelectionWidthInPixels;
+
+                if ((int)nextSelectionWidthInPixels != (int)previousSelectionWidthInPixels)
+                    stateHasChanged = true;
+            }
+
+            var widthCssStyleString = "width: ";
+
+            var elementMeasurements = RenderBatch.ViewModel!.VirtualizationResult.TextEditorMeasurements;
+
+            var fullWidthValue = elementMeasurements.ScrollWidth;
+
+            if (elementMeasurements.Width > elementMeasurements.ScrollWidth)
+                fullWidthValue = elementMeasurements.Width; // If content does not fill the viewable width of the Text Editor User Interface
+
+            var fullWidthValueInPixelsInvariantCulture = fullWidthValue.ToCssValue();
+
+            var selectionWidthInPixelsInvariantCulture = _selectionWidthInPixels.ToCssValue();
+
+            if (fullWidthOfRowIsSelected)
+                widthCssStyleString += $"{fullWidthValueInPixelsInvariantCulture}px;";
+            else if (selectionStartingColumnIndex != 0 && upperPositionIndexExclusive > line.EndPositionIndexExclusive - 1)
+                widthCssStyleString += $"calc({fullWidthValueInPixelsInvariantCulture}px - {selectionStartInPixelsInvariantCulture}px);";
+            else
+                widthCssStyleString += $"{selectionWidthInPixelsInvariantCulture}px;";
+
+            if (stateHasChanged)
+                await InvokeAsync(StateHasChanged);
+
+            _proportionalTextSelectionStyleCss = $"{top} {height} {left} {widthCssStyleString}";
         }
-
-        var widthCssStyleString = "width: ";
-
-        var elementMeasurements = RenderBatch.ViewModel!.VirtualizationResult.TextEditorMeasurements;
-
-        var fullWidthValue = elementMeasurements.ScrollWidth;
-
-        if (elementMeasurements.Width > elementMeasurements.ScrollWidth)
-            fullWidthValue = elementMeasurements.Width; // If content does not fill the viewable width of the Text Editor User Interface
-
-        var fullWidthValueInPixelsInvariantCulture = fullWidthValue.ToCssValue();
-
-        var selectionWidthInPixelsInvariantCulture = _selectionWidthInPixels.ToCssValue();
-
-        if (fullWidthOfRowIsSelected)
-            widthCssStyleString += $"{fullWidthValueInPixelsInvariantCulture}px;";
-        else if (selectionStartingColumnIndex != 0 && upperPositionIndexExclusive > line.EndPositionIndexExclusive - 1)
-            widthCssStyleString += $"calc({fullWidthValueInPixelsInvariantCulture}px - {selectionStartInPixelsInvariantCulture}px);";
-        else
-            widthCssStyleString += $"{selectionWidthInPixelsInvariantCulture}px;";
-
-        if (stateHasChanged)
-            await InvokeAsync(StateHasChanged);
-
-        _proportionalTextSelectionStyleCss = $"{top} {height} {left} {widthCssStyleString}";
+        catch (LuthetusTextEditorException)
+        {
+            // Eat the exception
+        }
     }
 }
