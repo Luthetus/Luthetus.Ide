751ddaf74b34d89997958f43d7797930ba1c2176
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-05-10 12:27:47 -0400

    ugh

--- Source/Lib/Common/Drags/Displays/DragInitializer.razor.cs
+++ Source/Lib/Common/Drags/Displays/DragInitializer.razor.cs
@@ -38,7 +38,7 @@ public partial class DragInitializer : FluxorComponent
         });
     }
 
-    private async Task DispatchSetDragStateActionOnMouseMoveAsync(MouseEventArgs mouseEventArgs)
+    private Task DispatchSetDragStateActionOnMouseMoveAsync(MouseEventArgs mouseEventArgs)
     {
         /*
          * This scenario is a perfect example of the issue I'm facing (2024-05-10)
@@ -118,9 +118,132 @@ public partial class DragInitializer : FluxorComponent
          *          -But again, who is going to await the BlockingCollection?
          *               -If I use a Task.Run(async () => await BlockingCollection...()), I have no idea
          *                    when this will be executed, right?
+         * ==========================================================================
+         * 
+         * I just ran the 5 second throttle on ServerSide and it seems to invoke the task,
+         * with no regard for the 5 second throttle at all.
+         * 
+         * I think I'm my own worst enemy here. I keep writing the throttle to work for
+         * either WASM or ServerSide, then I swap to the other, it doesn't work,
+         * so I re-write, then swap, it doesn't work, re-write, etc.....
+         * 
+         * I need to just understand how threads work.
+         * 
+         * I'm wondering, I currently have '.ConfigureAwait(false);'.
+         * So, is it the case that WASM is single threaded,
+         * therefore '.ConfigureAwait(false);' has no impact in this situation,
+         * with or without its stuck for 5 seconds on the delay.
+         * 
+         * Where as:
+         * If ServerSide is multi-threaded '.ConfigureAwait(false);', tells the
+         * thread that the 'PushEvent' can be ran on a different 'thread/context'.
+         * This allows the 5 second delay to be awaited on a thread different
+         * than the UI thread.
+         * 
+         * If I remove the '.ConfigureAwait(false);' is ServerSide going to
+         * await the 5 second delay with the UI thread when running ServerSide?
+         * 
+         * Further confusion comes from single threaded code having a concurrency
+         * model that permits the illusion of code running in parallel.
+         * But I should do like I said, remove that configure await and see what happens.
+         * 
+         * Okay, on WASM. I ran with '.ConfigureAwait(false);', and without,
+         * at a 5 second delay. In both situations the UI was locked
+         * for those 5 seconds of delay.
+         * 
+         * Now for ServerSide...
+         *     -With '.ConfigureAwait(false);'
+         *          the UI got blocked for those 5 seconds of delay.
+         *     -Without the '.ConfigureAwait(false);'
+         *          the UI got blocked for those 5 seconds of delay.
+         *          
+         * I'm thoroughly confused now because I just ran ServerSide
+         * with the '.ConfigureAwait(false);' about 5 minutes ago,
+         * and the UI wasn't getting blocked for the 5 seconds of delay.
+         * 
+         * I think I ran it with Release mode during that 5 minutes ago period.
+         * I also wonder if I somehow forgot to re-run the ServerSide application,
+         * and am running an old version that doesn't have '.ConfigureAwait(false);'?
+         * 
+         * I re-ran the ServerSide app with the '.ConfigureAwait(false);'
+         * to be sure that I was running the correct code.
+         * It still locks for the 5 seconds of delay.
+         * 
+         * I'm going to try Release mode...
+         * With Release mode and '.ConfigureAwait(false);' the
+         * delay is locking the UI.
+         * 
+         * I feel as though all I've done is type into this comment the entire time,
+         * and that I haven't touched anything, thereby am I crazy
+         * for having this memory of how 5+ minutes ago ServerSide with '.ConfigureAwait(false);'
+         * didn't have the delay locking the UI???
+         * 
+         * I checked git and beyond '.ConfigureAwait(false);' I do see that I had changed
+         * the method itself from being 'Task' to 'async Task' in order to await
+         * the configured Task.
+         * 
+         * From my understanding, if I use the 'async' keyword, then a state machine 
+         * is made for the method invocation.
+         * 
+         * I don't use the 'async' keyword, then someone upstream is expected to await
+         * my Task, and at that point the state machine is made.
+         * 
+         * Therefore, its beneficial for me to simply return a Task, as opposed to awaiting it,
+         * where possible. (to avoid the state machine overhead).
+         * 
+         * The reason I added the 'async' keyword, was because I don't think I can return
+         * a ConfiguredTaskAwaitable when the method signature says it will return a Task.
+         * 
+         * For this reason I had to be the one to await the ConfiguredTaskAwaitable.
+         * And turning the method to async, "implicitly-returns a task" so to speak.
+         * 
+         * I have no proof that I cannot return a ConfiguredTaskAwaitable,
+         * so I should try it now.
+         * 
+         * Trying to return 'ConfiguredTaskAwaitable' from a method with the return signature
+         * 'Task' gives an error that 'ConfiguredTaskAwaitable' is not implicitly convertable
+         * to 'Task'.
+         * 
+         * ==================================================================================
+         * 
+         * Well, it seems there are 3 cases foreach hosting model:
+         *     -Return the Task (no async keyword)
+         *     -Await the Task (with async keyword)
+         *     -Await the ConfiguredTaskAwaitable (with async keyword)
+         *     
+         * I guess I want a checklist so I'm going to type out all 6 cases:
+         *     [ ] WASM
+         *          [ ] Return the Task (no async keyword)
+         *          [ ] Await the Task (with async keyword)
+         *          [ ] Await the ConfiguredTaskAwaitable (with async keyword)
+         *     [ ] ServerSide
+         *          [ ] Return the Task (no async keyword)
+         *          [ ] Await the Task (with async keyword)
+         *          [ ] Await the ConfiguredTaskAwaitable (with async keyword)
+         *          
+         * Beyond this though, I've written the Throttle logic
+         * quite a few times, and these various times were done differently.
+         * 
+         * I should keep handy the various ways I've written the throttle.
+         * Then I can add an even further top-hierarchy to my checklist,
+         * which would be the implementation I went for with the throttle.
+         * 
+         * ==================================================================================
+         * 
+         * A side thought in my head: if WASM is single threaded, and by way of a
+         * concurrency model (async-await) one can create the illusion of parallelism
+         * with a single thread, why did I earlier today see no changes when using
+         * '.ConfigureAwait(false)' with the WASM host, vs not using it?
+         * Presumably, the "single threaded illusion" here would be the task for the delay
+         * starting, then the UI thread takes control of the thread until its somehow
+         * notified that the delay if over?
+         * 
+         * I need to pay attention and see if I can find any similarities/differences between
+         * using '.ConfigureAwait(false)' when running with a single threaded application vs multi threaded.
+         * (further side note: is it single threaded runtime or single threaded application?)
          */
 
-        await _throttleDispatchSetDragStateActionOnMouseMove.PushEvent(_ =>
+        return _throttleDispatchSetDragStateActionOnMouseMove.PushEvent(_ =>
         {
             if ((mouseEventArgs.Buttons & 1) != 1)
             {
