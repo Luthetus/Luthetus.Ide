bca1303a977ea40b62f9896a2da6970ddd8c16a1
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-05-10 14:00:07 -0400

    OnExecutionKindChanged

--- Source/Lib/Common/Reactives/Displays/CounterThrottleAsyncDisplay.razor.cs
+++ Source/Lib/Common/Reactives/Displays/CounterThrottleAsyncDisplay.razor.cs
@@ -7,8 +7,44 @@ public partial class CounterThrottleAsyncDisplay : ComponentBase
 {
     private readonly CounterThrottleAsync _counterThrottleAsync = new CounterThrottleAsync();
 
-    private void FireThrottleOnClick()
-    {
+    private int _count;
+    private List<Func<Task>>? _emptyWorkItemList;
+    private List<CounterThrottleAsync.ExecutionKind>? _executionKindList;
 
+    private List<Func<Task>> EmptyWorkItemList => _emptyWorkItemList ??= new();
+    private List<CounterThrottleAsync.ExecutionKind> ExecutionKindList => _executionKindList ??= Enum.GetValues<CounterThrottleAsync.ExecutionKind>().ToList();
+
+    private Task FireThrottleOnClick()
+    {
+        return _counterThrottleAsync.PushEvent(() =>
+        {
+            _count++;
+            return Task.CompletedTask;
+        });
+    }
+
+    private bool TryGetWorkItemList(out List<Func<Task>> workItemList)
+    {
+        try
+        {
+            workItemList = _counterThrottleAsync._workItemStack.ToList();
+            return true;
+        }
+        catch (Exception)
+        {
+            workItemList = EmptyWorkItemList;
+            return false;
+        }
+    }
+
+    private async Task OnExecutionKindChanged(ChangeEventArgs changeEventArgs)
+    {
+        if (changeEventArgs.Value is null)
+            return;
+
+        var executionKindString = (string)changeEventArgs.Value;
+
+        if (Enum.TryParse<CounterThrottleAsync.ExecutionKind>(executionKindString, out var selectedExecutionKind))
+            await _counterThrottleAsync.SetExecuteKind(selectedExecutionKind);
     }
 }
--- Source/Lib/Common/Reactives/Models/Internals/CounterThrottleAsync.cs
+++ Source/Lib/Common/Reactives/Models/Internals/CounterThrottleAsync.cs
@@ -5,11 +5,94 @@ public class CounterThrottleAsync : ICounterThrottleAsync
     public readonly SemaphoreSlim _workItemSemaphore = new(1, 1);
     public readonly Stack<Func<Task>> _workItemStack = new();
 
+    public Task _delayTask = Task.CompletedTask;
+    public Task _workItemTask = Task.CompletedTask;
+    public ExecutionKind _executionKind = ExecutionKind.Await;
+
     public TimeSpan ThrottleTimeSpan { get; }
+
+    public async Task SetExecuteKind(ExecutionKind executionKind)
+    {
+        try
+        {
+            await _workItemSemaphore.WaitAsync().ConfigureAwait(false);
+
+            _executionKind = executionKind;
+        }
+        finally
+        {
+            _workItemSemaphore.Release();
+        }
+    }
 
     public async Task PushEvent(Func<Task> workItem)
     {
-        // Push workItem onto stack
+        var localExecutionKind = _executionKind;
+
+        await Push(workItem);
+
+        switch (localExecutionKind)
+        {
+            case ExecutionKind.Await:
+                await Execute_Await();
+                break;
+            case ExecutionKind.TaskRun:
+                await Execute_TaskRun();
+                break;
+            default:
+                throw new NotImplementedException($"The {nameof(ExecutionKind)}: '{localExecutionKind}' was not recognized.");
+        }
+    }
+
+    public async Task Execute_Await()
+    {
+        Func<Task> popWorkItem;
+        try
+        {
+            await _workItemSemaphore.WaitAsync().ConfigureAwait(false);
+
+            popWorkItem = _workItemStack.Pop();
+            _workItemStack.Clear();
+        }
+        finally
+        {
+            _workItemSemaphore.Release();
+        }
+
+        _delayTask = Task.Delay(ThrottleTimeSpan);
+        _workItemTask = popWorkItem.Invoke();
+
+        await Task.WhenAll(_delayTask, _workItemTask);
+    }
+
+    public async Task Execute_TaskRun()
+    {
+        Func<Task> popWorkItem;
+        try
+        {
+            await _workItemSemaphore.WaitAsync().ConfigureAwait(false);
+
+            popWorkItem = _workItemStack.Pop();
+            _workItemStack.Clear();
+        }
+        finally
+        {
+            _workItemSemaphore.Release();
+        }
+
+        _delayTask = Task.Run(async () => await Task.Delay(ThrottleTimeSpan));
+        _workItemTask = Task.Run(async () => await popWorkItem.Invoke());
+    }
+
+    /// <summary>
+    /// The goal of this class is to visualize various attempts of implementing
+    /// a throttle.<br/><br/>
+    /// 
+    /// Its presumed that all the versions will have this code be equivalent,
+    /// so moving it into its own method helps for readability.
+    /// </summary>
+    private async Task Push(Func<Task> workItem)
+    {
         try
         {
             await _workItemSemaphore.WaitAsync().ConfigureAwait(false);
@@ -22,5 +105,11 @@ public class CounterThrottleAsync : ICounterThrottleAsync
         {
             _workItemSemaphore.Release();
         }
+    }
+
+    public enum ExecutionKind
+    {
+        Await,
+        TaskRun,
     }
 }
