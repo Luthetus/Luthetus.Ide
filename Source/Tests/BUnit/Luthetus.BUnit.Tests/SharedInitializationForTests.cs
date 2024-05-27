5e64f016f20a495eeccc8de4930dc01d973ee767
Hunter Freeman <luthetus@gmail.com>
2024-05-27 10:25:34 -0400

    BackgroundTasksHandle.cs for unit tests

--- Source/Tests/BUnit/Luthetus.BUnit.Tests/SharedInitializationForTests.cs
+++ Source/Tests/BUnit/Luthetus.BUnit.Tests/SharedInitializationForTests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Fluxor;
 using Luthetus.Common.RazorLib.BackgroundTasks.Models;
 using Luthetus.Common.RazorLib.Installations.Models;
@@ -42,5 +45,60 @@ public static class SharedInitializationForTests
 		}
 
 		var storeInitializer = ctx.RenderComponent<Fluxor.Blazor.Web.StoreInitializer>();
+	}
+
+	public static BackgroundTasksHandle StartBackgroundTasks(TestContext ctx)
+	{
+		return new BackgroundTasksHandle(ctx);
+	}
+
+	public class BackgroundTasksHandle
+	{
+		public BackgroundTasksHandle(TestContext ctx)
+		{
+			ContinuousBtw = ctx.Services.GetRequiredService<ContinuousBackgroundTaskWorker>();
+			ContinuousStartTask = ContinuousBtw.StartAsync(ContinuousStartCts.Token);
+
+			BlockingBtw = ctx.Services.GetRequiredService<BlockingBackgroundTaskWorker>();
+			BlockingStartTask = BlockingBtw.StartAsync(BlockingStartCts.Token);
+
+			AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+	        {
+	            ContinuousStartCts.Cancel();
+	            BlockingStartCts.Cancel();
+	
+	            ContinuousStopTask = ContinuousBtw.StopAsync(ContinuousStopCts.Token);
+	            BlockingStopTask = BlockingBtw.StopAsync(BlockingStopCts.Token);
+	        };
+	
+	        AppDomain.CurrentDomain.ProcessExit += (sender, error) =>
+	        {
+	            ContinuousStartCts.Cancel();
+	            BlockingStartCts.Cancel();
+	
+	            ContinuousStopCts.Cancel();
+	            BlockingStopCts.Cancel();
+	        };
+		}
+
+		public CancellationTokenSource ContinuousStartCts { get; } = new();
+        public CancellationTokenSource ContinuousStopCts { get; } = new();
+		public ContinuousBackgroundTaskWorker ContinuousBtw { get; }
+        public Task ContinuousStartTask { get; }
+		public Task? ContinuousStopTask { get; private set; }
+
+        public CancellationTokenSource BlockingStartCts { get; } = new();
+        public CancellationTokenSource BlockingStopCts { get; } = new();
+        public BlockingBackgroundTaskWorker BlockingBtw { get; }
+        public Task BlockingStartTask { get; }
+        public Task? BlockingStopTask { get; private set; }
+
+		public Task Stop()
+		{
+			ContinuousStopTask = ContinuousBtw.StopAsync(ContinuousStopCts.Token);
+	        BlockingStopTask = BlockingBtw.StopAsync(BlockingStopCts.Token);
+
+			return Task.WhenAll(ContinuousStopTask, BlockingStopTask);
+		}
 	}
 }
