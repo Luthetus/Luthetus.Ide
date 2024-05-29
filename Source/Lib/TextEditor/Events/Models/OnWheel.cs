958523e76dafece301bdbb96c103965f7708b5c1
Hunter Freeman <luthetus@gmail.com>
2024-05-28 21:50:18 -0400

    Linux horizontal wheel scrolling

--- Source/Lib/TextEditor/Events/Models/OnWheel.cs
+++ Source/Lib/TextEditor/Events/Models/OnWheel.cs
@@ -35,78 +35,170 @@ public class OnWheel : ITextEditorTask
 
     public IBackgroundTask? BatchOrDefault(IBackgroundTask oldEvent)
     {
+		// Horizontal mouse wheel was not working on Linux.
+		// Prior to the fix that was just made, only the 'WheelEventArgs.DeltaY'
+		// was being used.
+		//
+		// Reason being, on Windows, regardless of if 'WheelEventArgs.ShiftKey' was true or false,
+		// the 'WheelEventArgs.DeltaY' was always being provided as a non-zero value.
+		//
+		// But, on Linux, when not holding the shift key: 'WheelEventArgs.DeltaY' is set,
+		// and 'WheelEventArgs.DeltaX' is 0.
+		// The vice versa though, if one does hold the shift key, 'WheelEventArgs.DeltaY' is zero,
+		// and 'WheelEventArgs.DeltaX' is set.
+		//
+		// If I end up breaking Windows with this change (to use ether 'WheelEventArgs.DeltaY'
+		// or 'WheelEventArgs.DeltaX' dependent on 'WheelEventArgs.ShiftKey',
+		// then... I'm tired and forgot what I was typing. (2024-05-28)
+
+
         // If the two individuals, or a batch and an individual are both positive,
         // then batch them, etc... for negative and 0
 
         if (oldEvent is OnWheel oldEventOnWheel)
         {
-            if (oldEventOnWheel.WheelEventArgs.DeltaY > 0 &&
-                WheelEventArgs.DeltaY > 0)
-            {
-                return new OnWheelBatch(
-                    new List<WheelEventArgs>()
-                    {
-                        oldEventOnWheel.WheelEventArgs,
-                        WheelEventArgs
-                    },
-                    _events,
-                    ViewModelKey)
-					{
-						EditContext = EditContext
-					};
-            }
-            else if (oldEventOnWheel.WheelEventArgs.DeltaY < 0 &&
-                     WheelEventArgs.DeltaY < 0)
-            {
-                return new OnWheelBatch(
-                    new List<WheelEventArgs>()
-                    {
-                        oldEventOnWheel.WheelEventArgs,
-                        WheelEventArgs
-                    },
-                    _events,
-                    ViewModelKey)
-					{
-						EditContext = EditContext
-					};
-            }
-            else if (oldEventOnWheel.WheelEventArgs.DeltaY == 0 &&
-                     WheelEventArgs.DeltaY == 0)
-            {
-                return new OnWheelBatch(
-                    new List<WheelEventArgs>()
-                    {
-                        oldEventOnWheel.WheelEventArgs,
-                        WheelEventArgs
-                    },
-                    _events,
-                    ViewModelKey)
-					{
-						EditContext = EditContext
-					};
-            }
+			if (oldEventOnWheel.WheelEventArgs.ShiftKey && WheelEventArgs.ShiftKey)
+			{
+	            if (oldEventOnWheel.WheelEventArgs.DeltaX > 0 &&
+	                WheelEventArgs.DeltaX > 0)
+	            {
+	                return new OnWheelBatch(
+	                    new List<WheelEventArgs>()
+	                    {
+	                        oldEventOnWheel.WheelEventArgs,
+	                        WheelEventArgs
+	                    },
+	                    _events,
+	                    ViewModelKey)
+						{
+							EditContext = EditContext
+						};
+	            }
+	            else if (oldEventOnWheel.WheelEventArgs.DeltaX < 0 &&
+	                     WheelEventArgs.DeltaX < 0)
+	            {
+	                return new OnWheelBatch(
+	                    new List<WheelEventArgs>()
+	                    {
+	                        oldEventOnWheel.WheelEventArgs,
+	                        WheelEventArgs
+	                    },
+	                    _events,
+	                    ViewModelKey)
+						{
+							EditContext = EditContext
+						};
+	            }
+	            else if (oldEventOnWheel.WheelEventArgs.DeltaX == 0 &&
+	                     WheelEventArgs.DeltaX == 0)
+	            {
+	                return new OnWheelBatch(
+	                    new List<WheelEventArgs>()
+	                    {
+	                        oldEventOnWheel.WheelEventArgs,
+	                        WheelEventArgs
+	                    },
+	                    _events,
+	                    ViewModelKey)
+						{
+							EditContext = EditContext
+						};
+	            }
+			}
+			else if (!oldEventOnWheel.WheelEventArgs.ShiftKey && !WheelEventArgs.ShiftKey)
+			{
+	            if (oldEventOnWheel.WheelEventArgs.DeltaY > 0 &&
+	                WheelEventArgs.DeltaY > 0)
+	            {
+	                return new OnWheelBatch(
+	                    new List<WheelEventArgs>()
+	                    {
+	                        oldEventOnWheel.WheelEventArgs,
+	                        WheelEventArgs
+	                    },
+	                    _events,
+	                    ViewModelKey)
+						{
+							EditContext = EditContext
+						};
+	            }
+	            else if (oldEventOnWheel.WheelEventArgs.DeltaY < 0 &&
+	                     WheelEventArgs.DeltaY < 0)
+	            {
+	                return new OnWheelBatch(
+	                    new List<WheelEventArgs>()
+	                    {
+	                        oldEventOnWheel.WheelEventArgs,
+	                        WheelEventArgs
+	                    },
+	                    _events,
+	                    ViewModelKey)
+						{
+							EditContext = EditContext
+						};
+	            }
+	            else if (oldEventOnWheel.WheelEventArgs.DeltaY == 0 &&
+	                     WheelEventArgs.DeltaY == 0)
+	            {
+	                return new OnWheelBatch(
+	                    new List<WheelEventArgs>()
+	                    {
+	                        oldEventOnWheel.WheelEventArgs,
+	                        WheelEventArgs
+	                    },
+	                    _events,
+	                    ViewModelKey)
+						{
+							EditContext = EditContext
+						};
+	            }
+			}
         }
 
         if (oldEvent is OnWheelBatch oldEventOnWheelBatch)
         {
-            if (oldEventOnWheelBatch.WheelEventArgsList.Last().DeltaY > 0 &&
+			if (oldEventOnWheelBatch.WheelEventArgsList.Last().ShiftKey && WheelEventArgs.ShiftKey)
+			{
+				if (oldEventOnWheelBatch.WheelEventArgsList.Last().DeltaX > 0 &&
+                WheelEventArgs.DeltaX > 0)
+	            {
+	                oldEventOnWheelBatch.WheelEventArgsList.Add(WheelEventArgs);
+	                return oldEventOnWheelBatch;
+	            }
+	            else if (oldEventOnWheelBatch.WheelEventArgsList.Last().DeltaX < 0 &&
+	                     WheelEventArgs.DeltaX < 0)
+	            {
+	                oldEventOnWheelBatch.WheelEventArgsList.Add(WheelEventArgs);
+	                return oldEventOnWheelBatch;
+	            }
+	            else if (oldEventOnWheelBatch.WheelEventArgsList.Last().DeltaX == 0 &&
+	                     WheelEventArgs.DeltaX == 0)
+	            {
+	                oldEventOnWheelBatch.WheelEventArgsList.Add(WheelEventArgs);
+	                return oldEventOnWheelBatch;
+	            }
+			}
+   	     else if (!oldEventOnWheelBatch.WheelEventArgsList.Last().ShiftKey && !WheelEventArgs.ShiftKey)
+			{
+				if (oldEventOnWheelBatch.WheelEventArgsList.Last().DeltaY > 0 &&
                 WheelEventArgs.DeltaY > 0)
-            {
-                oldEventOnWheelBatch.WheelEventArgsList.Add(WheelEventArgs);
-                return oldEventOnWheelBatch;
-            }
-            else if (oldEventOnWheelBatch.WheelEventArgsList.Last().DeltaY < 0 &&
-                     WheelEventArgs.DeltaY < 0)
-            {
-                oldEventOnWheelBatch.WheelEventArgsList.Add(WheelEventArgs);
-                return oldEventOnWheelBatch;
-            }
-            else if (oldEventOnWheelBatch.WheelEventArgsList.Last().DeltaY == 0 &&
-                     WheelEventArgs.DeltaY == 0)
-            {
-                oldEventOnWheelBatch.WheelEventArgsList.Add(WheelEventArgs);
-                return oldEventOnWheelBatch;
-            }
+	            {
+	                oldEventOnWheelBatch.WheelEventArgsList.Add(WheelEventArgs);
+	                return oldEventOnWheelBatch;
+	            }
+	            else if (oldEventOnWheelBatch.WheelEventArgsList.Last().DeltaY < 0 &&
+	                     WheelEventArgs.DeltaY < 0)
+	            {
+	                oldEventOnWheelBatch.WheelEventArgsList.Add(WheelEventArgs);
+	                return oldEventOnWheelBatch;
+	            }
+	            else if (oldEventOnWheelBatch.WheelEventArgsList.Last().DeltaY == 0 &&
+	                     WheelEventArgs.DeltaY == 0)
+	            {
+	                oldEventOnWheelBatch.WheelEventArgsList.Add(WheelEventArgs);
+	                return oldEventOnWheelBatch;
+	            }
+			}
         }
 
         return null;
@@ -123,7 +215,7 @@ public class OnWheel : ITextEditorTask
             if (WheelEventArgs.ShiftKey)
             {
                 await EditContext.TextEditorService.ViewModelApi
-                    .MutateScrollHorizontalPositionFactory(ViewModelKey, WheelEventArgs.DeltaY)
+                    .MutateScrollHorizontalPositionFactory(ViewModelKey, WheelEventArgs.DeltaX)
                     .Invoke(EditContext);
             }
             else
