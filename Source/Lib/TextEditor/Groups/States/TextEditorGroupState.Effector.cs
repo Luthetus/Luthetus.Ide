d558b6678cb627d043ea6b9d73526da5194524ef
Hunter Freeman <luthetus@gmail.com>
2024-06-10 16:33:07 -0400

    Scroll when adding or changing tabs in the text editor

---
+++ Source/Lib/TextEditor/Groups/States/TextEditorGroupState.Effector.cs
@@ -0,0 +1,58 @@
+using System.Collections.Immutable;
+using Fluxor;
+using Luthetus.Common.RazorLib.Keys.Models;
+using Luthetus.TextEditor.RazorLib.TextEditors.Models;
+using Luthetus.TextEditor.RazorLib.Groups.Models;
+using Luthetus.TextEditor.RazorLib.Commands.Models.Defaults;
+using Luthetus.TextEditor.RazorLib.Lexes.Models;
+using Luthetus.TextEditor.RazorLib.Commands.Models;
+
+namespace Luthetus.TextEditor.RazorLib.Groups.States;
+
+public partial class TextEditorGroupState
+{
+	public class Effector
+	{
+		private readonly ITextEditorService _textEditorService;
+
+		public Effector(ITextEditorService textEditorService)
+		{
+			_textEditorService = textEditorService;
+		}
+
+		[EffectMethod]
+		public Task HandleAddViewModelToGroupAction(
+			AddViewModelToGroupAction addViewModelToGroupAction,
+			IDispatcher dispatcher)
+		{
+			return PostScroll(addViewModelToGroupAction.GroupKey, addViewModelToGroupAction.ViewModelKey);
+		}
+
+		[EffectMethod]
+		public Task HandleSetActiveViewModelOfGroupAction(
+			SetActiveViewModelOfGroupAction setActiveViewModelOfGroupAction,
+			IDispatcher dispatcher)
+		{
+			return PostScroll(setActiveViewModelOfGroupAction.GroupKey, setActiveViewModelOfGroupAction.ViewModelKey);
+		}
+
+		private Task PostScroll(
+			Key<TextEditorGroup> groupKey,
+        	Key<TextEditorViewModel> viewModelKey)
+		{
+			return _textEditorService.PostTakeMostRecent(
+				nameof(PostScroll),
+				new ResourceUri(string.Empty),
+				viewModelKey,
+				editContext =>
+				{
+					var viewModelModifier = editContext.GetViewModelModifier(viewModelKey);
+		            if (viewModelModifier is null)
+		                return Task.CompletedTask;
+
+        			viewModelModifier.ScrollWasModified = true;
+					return Task.CompletedTask;
+				});
+		}
+	}
+}
