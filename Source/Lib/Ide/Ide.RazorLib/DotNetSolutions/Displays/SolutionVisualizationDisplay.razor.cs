36ccf6801242740692ccf7647c0fcaffe12c2346
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-06-21 12:55:38 -0400

    simple start to SolutionVisualizationDisplay.razor

--- Source/Lib/Ide/Ide.RazorLib/DotNetSolutions/Displays/SolutionVisualizationDisplay.razor.cs
+++ Source/Lib/Ide/Ide.RazorLib/DotNetSolutions/Displays/SolutionVisualizationDisplay.razor.cs
@@ -1,8 +1,38 @@
 using Microsoft.AspNetCore.Components;
+using Luthetus.TextEditor.RazorLib.CompilerServices.Interfaces;
+using Luthetus.CompilerServices.Lang.CSharp.CompilerServiceCase;
+using Luthetus.Ide.RazorLib.CompilerServices.Models;
 
 namespace Luthetus.Ide.RazorLib.DotNetSolutions.Displays;
 
-public partial class SolutionVisualizationDisplay : ComponentBase
+public partial class SolutionVisualizationDisplay : ComponentBase, IDisposable
 {
-	
+	[Inject]
+	private ICompilerServiceRegistry InterfaceCompilerServiceRegistry { get; set; } = null!;
+
+	private CSharpCompilerService _cSharpCompilerService;
+
+	protected override void OnInitialized()
+	{
+		_cSharpCompilerService = ((CompilerServiceRegistry)InterfaceCompilerServiceRegistry)
+			.CSharpCompilerService;
+
+	    _cSharpCompilerService.ResourceRegistered += OnCompilerServiceChanged;
+	    _cSharpCompilerService.ResourceParsed += OnCompilerServiceChanged;
+	    _cSharpCompilerService.ResourceDisposed += OnCompilerServiceChanged;
+
+		base.OnInitialized();
+	}
+
+	public async void OnCompilerServiceChanged()
+	{
+		await InvokeAsync(StateHasChanged);
+	}
+
+	public void Dispose()
+	{
+		_cSharpCompilerService.ResourceRegistered -= OnCompilerServiceChanged;
+	    _cSharpCompilerService.ResourceParsed -= OnCompilerServiceChanged;
+	    _cSharpCompilerService.ResourceDisposed -= OnCompilerServiceChanged;
+	}
 }
