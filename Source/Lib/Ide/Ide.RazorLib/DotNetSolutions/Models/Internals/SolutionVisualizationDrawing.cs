a942b50256234dd3c020441bf27de7a4dead1b5b
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-06-23 13:23:49 -0400

    Add: LoadClasses(...)

--- Source/Lib/Ide/Ide.RazorLib/DotNetSolutions/Models/Internals/SolutionVisualizationDrawing.cs
+++ Source/Lib/Ide/Ide.RazorLib/DotNetSolutions/Models/Internals/SolutionVisualizationDrawing.cs
@@ -4,12 +4,15 @@ using Fluxor;
 using Luthetus.Common.RazorLib.Menus.Models;
 using Luthetus.Common.RazorLib.FileSystems.Models;
 using Luthetus.Common.RazorLib.Keys.Models;
+using Luthetus.Common.RazorLib.Notifications.Models;
+using Luthetus.Common.RazorLib.ComponentRenderers.Models;
 using Luthetus.TextEditor.RazorLib.Lexes.Models;
 using Luthetus.TextEditor.RazorLib.CompilerServices.Interfaces;
 using Luthetus.TextEditor.RazorLib.Installations.Models;
 using Luthetus.TextEditor.RazorLib.TextEditors.Models;
 using Luthetus.TextEditor.RazorLib.Groups.Models;
 using Luthetus.CompilerServices.Lang.DotNetSolution.CompilerServiceCase;
+using Luthetus.CompilerServices.Lang.CSharpProject.CompilerServiceCase;
 using Luthetus.Ide.RazorLib.DotNetSolutions.States;
 
 namespace Luthetus.Ide.RazorLib.DotNetSolutions.Models.Internals;
@@ -56,6 +59,16 @@ public class SolutionVisualizationDrawing<TItem> : ISolutionVisualizationDrawing
 				    MenuOptionKind.Other,
 					OnClickFunc: () => LoadProjects(
 						(DotNetSolutionResource)compilerServiceResource,
+						textEditorConfig,
+						serviceProvider)));			
+			}
+			else if (SolutionVisualizationDrawingKind == SolutionVisualizationDrawingKind.Project)
+			{
+				menuOptionRecordList.Add(new MenuOptionRecord(
+				    "Load Classes",
+				    MenuOptionKind.Other,
+					OnClickFunc: () => LoadClasses(
+						(CSharpProjectResource)compilerServiceResource,
 						textEditorConfig,
 						serviceProvider)));			
 			}
@@ -126,28 +139,75 @@ public class SolutionVisualizationDrawing<TItem> : ISolutionVisualizationDrawing
 	                serviceProvider))
 	            .ConfigureAwait(false);
 		}
-		
-		/*
-		if (textEditorConfig.TryRegisterViewModelFunc is not null)
+	}
+
+	private async Task LoadClasses(
+		CSharpProjectResource cSharpProjectResource,
+		LuthetusTextEditorConfig textEditorConfig,
+		IServiceProvider serviceProvider)
+	{
+        if (textEditorConfig.RegisterModelFunc is null)
+            return;
+
+		var environmentProvider = serviceProvider.GetRequiredService<IEnvironmentProvider>();
+		var fileSystemProvider = serviceProvider.GetRequiredService<IFileSystemProvider>();
+		var commonComponentRenderers = serviceProvider.GetRequiredService<ILuthetusCommonComponentRenderers>();
+		var dispatcher = serviceProvider.GetRequiredService<IDispatcher>();
+
+		var projectAbsolutePath = environmentProvider.AbsolutePathFactory(cSharpProjectResource.ResourceUri.Value, false);
+
+		var parentDirectory = projectAbsolutePath.ParentDirectory;
+		if (parentDirectory is null)
+	        return;
+
+		var startingAbsolutePathForSearch = parentDirectory.Value;
+
+		var discoveredFileList = new List<string>();
+
+		await DiscoverFilesRecursively(startingAbsolutePathForSearch, discoveredFileList).ConfigureAwait(false);
+
+		NotificationHelper.DispatchInformative(
+	        "SolutionVisualizationDrawing",
+	        $"{projectAbsolutePath.NameWithExtension} discovery {discoveredFileList.Count} C# files",
+	        commonComponentRenderers,
+	        dispatcher,
+	        TimeSpan.FromSeconds(60));
+
+		foreach (var file in discoveredFileList)
+		{
+			var resourceUri = new ResourceUri(file);
+
+	        await textEditorConfig.RegisterModelFunc.Invoke(new RegisterModelArgs(
+	                resourceUri,
+	                serviceProvider))
+	            .ConfigureAwait(false);
+		}
+
+        async Task DiscoverFilesRecursively(string directoryPathParent, List<string> discoveredFileList)
         {
-            var viewModelKey = await textEditorConfig.TryRegisterViewModelFunc.Invoke(new TryRegisterViewModelArgs(
-                    Key<TextEditorViewModel>.NewKey(),
-                    dotNetSolutionResource.ResourceUri,
-                    new Category("main"),
-                    false,
-                    serviceProvider))
+            var directoryPathChildList = await fileSystemProvider.Directory.GetDirectoriesAsync(
+                    directoryPathParent,
+                    CancellationToken.None)
                 .ConfigureAwait(false);
 
-            if (viewModelKey != Key<TextEditorViewModel>.Empty &&
-                textEditorConfig.TryShowViewModelFunc is not null)
+            var filePathChildList = await fileSystemProvider.Directory.GetFilesAsync(
+                    directoryPathParent,
+                    CancellationToken.None)
+                .ConfigureAwait(false);
+
+            foreach (var filePathChild in filePathChildList)
             {
-                await textEditorConfig.TryShowViewModelFunc.Invoke(new TryShowViewModelArgs(
-                        viewModelKey,
-                        Key<TextEditorGroup>.Empty,
-                        serviceProvider))
-                    .ConfigureAwait(false);
+                if (filePathChild.EndsWith(".cs"))
+                    discoveredFileList.Add(filePathChild);
+            }
+
+            foreach (var directoryPathChild in directoryPathChildList)
+            {
+                if (directoryPathChild.Contains(".git") || directoryPathChild.Contains("bin") || directoryPathChild.Contains("obj"))
+                    continue;
+
+                await DiscoverFilesRecursively(directoryPathChild, discoveredFileList).ConfigureAwait(false);
             }
         }
-		*/
 	}
 }
