38c3d17e63a7bb99dc4728cd0e4793d574d823ff
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-07-09 13:44:19 -0400

    Better drawn context boundaries.

--- Source/Lib/Common/Contexts/Displays/ContextBoundary.razor.cs
+++ Source/Lib/Common/Contexts/Displays/ContextBoundary.razor.cs
@@ -1,12 +1,13 @@
+using System.Collections.Immutable;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using Fluxor;
 using Luthetus.Common.RazorLib.Commands.Models;
 using Luthetus.Common.RazorLib.Contexts.States;
 using Luthetus.Common.RazorLib.Contexts.Models;
 using Luthetus.Common.RazorLib.Keymaps.Models;
 using Luthetus.Common.RazorLib.Keys.Models;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using System.Collections.Immutable;
+using Luthetus.Common.RazorLib.Outlines.States;
 
 namespace Luthetus.Common.RazorLib.Contexts.Displays;
 
@@ -14,6 +15,15 @@ public partial class ContextBoundary : ComponentBase
 {
     [Inject]
     private IDispatcher Dispatcher { get; set; } = null!;
+    /// <summary>
+    /// Warning: Do not take lightly a future decision to have this type
+    ///          inherit FluxorComponent without noting that this injection will
+    ///          cause re-renders whenever the context state is changed.
+    /// </summary>
+    [Inject]
+    private IState<ContextState> ContextStateWrap { get; set; } = null!;
+    [Inject]
+    private IState<OutlineState> OutlineStateWrap { get; set; } = null!;
 
     [CascadingParameter]
     public ContextBoundary? ParentContextBoundary { get; set; }
@@ -29,6 +39,8 @@ public partial class ContextBoundary : ComponentBase
     public string StyleCssString { get; set; } = null!;
     [Parameter]
     public int TabIndex { get; set; } = -1;
+    
+    private int _shouldRemoveOutlineCount;
 
     public void DispatchSetActiveContextStatesAction(List<Key<ContextRecord>> contextRecordKeyList)
     {
@@ -41,8 +53,47 @@ public partial class ContextBoundary : ComponentBase
     }
 
     public void HandleOnFocusIn()
-    {
+    {    
+    	// TODO: There is a worry that the onfocusin could be redundantly setting the already
+    	//       existing 'FocusedContextHeirarchy' again, enough to where an optimization
+    	//       that checks to see if it already is the 'FocusedContextHeirarchy' would be of
+    	//       notable benefit.
+    	//
+    	if (ContextStateWrap.Value.FocusedContextHeirarchy.NearestAncestorKey != ContextRecord.ContextKey)
+    	{
+    	    Dispatcher.Dispatch(new OutlineState.SetOutlineAction(
+    	    	ContextRecord.ContextElementId,
+    	    	null,
+    	    	true));
+    	    	
+    	    _shouldRemoveOutlineCount = 1;
+    	}
+    	
         DispatchSetActiveContextStatesAction(new());
+    }
+    
+    private void HandleOnFocusOut()
+    {
+    	Dispatcher.Dispatch(new OutlineState.SetOutlineAction(
+	    	null,
+	    	null,
+	    	false));
+    }
+    
+    private void HandleOnClick()
+    {
+    	if (OutlineStateWrap.Value.ElementId == ContextRecord.ContextElementId &&
+    		_shouldRemoveOutlineCount == 0)
+    	{
+    		Dispatcher.Dispatch(new OutlineState.SetOutlineAction(
+		    	null,
+		    	null,
+		    	false));
+    	}
+    	else if (_shouldRemoveOutlineCount == 1)
+    	{
+    		_shouldRemoveOutlineCount--;
+    	}
     }
 
     public async Task HandleOnKeyDownAsync(KeyboardEventArgs keyboardEventArgs)
