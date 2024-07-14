73ed5e2961156de55e7d68279d42e11c79550ff3
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-07-14 18:14:18 -0400

    Fix: Tab context menu

--- Source/Lib/Common/Tabs/Displays/TabListDisplay.razor.cs
+++ Source/Lib/Common/Tabs/Displays/TabListDisplay.razor.cs
@@ -1,10 +1,10 @@
+using System.Collections.Immutable;
+using Microsoft.AspNetCore.Components;
 using Fluxor;
 using Luthetus.Common.RazorLib.Dropdowns.States;
 using Luthetus.Common.RazorLib.Dropdowns.Models;
 using Luthetus.Common.RazorLib.Dynamics.Models;
 using Luthetus.Common.RazorLib.Tabs.Models;
-using Microsoft.AspNetCore.Components;
-using System.Collections.Immutable;
 
 namespace Luthetus.Common.RazorLib.Tabs.Displays;
 
@@ -24,7 +24,7 @@ public partial class TabListDisplay : ComponentBase
 		await InvokeAsync(StateHasChanged);
 	}
 
-	private void HandleTabButtonOnContextMenu(TabContextMenuEventArgs tabContextMenuEventArgs)
+	private Task HandleTabButtonOnContextMenu(TabContextMenuEventArgs tabContextMenuEventArgs)
     {
 		var dropdownRecord = new DropdownRecord(
 			TabContextMenu.ContextMenuEventDropdownKey,
@@ -41,5 +41,6 @@ public partial class TabListDisplay : ComponentBase
 			restoreFocusOnClose: null);
 
         Dispatcher.Dispatch(new DropdownState.RegisterAction(dropdownRecord));
+        return Task.CompletedTask;
     }
 }
