b39e0261ea5d133767d8d2c989b3198a1df13e05
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-07-25 15:43:58 -0400

    net8.0; Fluxor v6; Photino v3;

--- External/Fluxor_fork/Source/Lib/Fluxor.Blazor.Web/ErrorMessages.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Fluxor.Blazor.Web
-{
-	internal class ErrorMessages
-	{
-		internal const string ForgottenToCallBaseOnInitialized =
-			"Have you forgotten to call base.OnInitialized() in your component?";
-	}
-}
