9e1e3d27517d6590aec0010b21891df21dc9f3c6
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-07-29 15:54:52 -0400

    RenderFragment

--- Source/Lib/TextEditor/TextEditors/Displays/Internals/WidgetLayerDisplay.razor.cs
+++ Source/Lib/TextEditor/TextEditors/Displays/Internals/WidgetLayerDisplay.razor.cs
@@ -9,7 +9,11 @@ public partial class WidgetLayerDisplay : ComponentBase
     [CascadingParameter]
     public TextEditorRenderBatchValidated RenderBatch { get; set; } = null!;
     
-    private string GetWidgetCssStyleBlock(TextEditorRenderBatchValidated localRenderBatch)
+    /// <summary>
+    /// This does not change per WidgetBlock rendered.
+    /// So, it should be invoked once and then re-used.
+    /// </summary>
+    private string GetBlockWidth(TextEditorRenderBatchValidated localRenderBatch)
     {
     	var widthInPixels = RenderBatch.ViewModel.TextEditorDimensions.Width -
             ScrollbarFacts.SCROLLBAR_SIZE_IN_PIXELS;
@@ -18,5 +22,22 @@ public partial class WidgetLayerDisplay : ComponentBase
         var width = $"width: {widthInPixelsInvariantCulture}px;";
 
         return width;
+    }
+    
+    private string GetBlockCssStyle(
+    	TextEditorRenderBatchValidated localRenderBatch,
+    	int lineIndex)
+    {
+    	var topInPixels = lineIndex * localRenderBatch.ViewModel.CharAndLineMeasurements.LineHeight;
+        var topInPixelsInvariantCulture = topInPixels.ToCssValue();
+        var top = $"top: {topInPixelsInvariantCulture}px;";
+        
+		var heightInPixels = localRenderBatch.ViewModel.TextEditorDimensions.Height -
+			localRenderBatch.ViewModel.CharAndLineMeasurements.LineHeight;
+			
+        var heightInPixelsInvariantCulture = heightInPixels.ToCssValue();
+        var height = $"height: {heightInPixelsInvariantCulture}px;";
+
+        return height + ' ' + top;
     }
 }
