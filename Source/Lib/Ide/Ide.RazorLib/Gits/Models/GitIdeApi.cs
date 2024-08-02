34a9e45e636da96f6cdde398cddf6ed193076d9f
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-08-01 20:23:10 -0400

    git pull

--- Source/Lib/Ide/Ide.RazorLib/Gits/Models/GitIdeApi.cs
+++ Source/Lib/Ide/Ide.RazorLib/Gits/Models/GitIdeApi.cs
@@ -530,21 +530,16 @@ public class GitIdeApi
                     Tag = GitCliOutputParser.TagConstants.PullEnqueue
 				};
 
-                var terminalCommand = new TerminalCommand(
-                    GitTerminalCommandKey,
-                    formattedCommand,
-                    localGitState.Repo.AbsolutePath.Value,
-                    OutputParser: _gitCliOutputParser,
-                    ContinueWith: () =>
-					{
-						RefreshEnqueue(repoAtTimeOfRequest);
-						return Task.CompletedTask;
-					});
-					
 				var terminalCommandRequest = new TerminalCommandRequest(
                 	formattedCommand.Value,
-                	localGitState.Repo.AbsolutePath.Value,
-                	Key<TerminalCommandRequest>.NewKey());
+                	localGitState.Repo.AbsolutePath.Value)
+                {
+                	ContinueWithFunc = parsedCommand =>
+                	{
+                		RefreshEnqueue(repoAtTimeOfRequest);
+						return Task.CompletedTask;
+                	}
+                };
                 	
                 _terminalStateWrap.Value.NEW_TERMINAL.EnqueueCommand(terminalCommandRequest);
 				return Task.CompletedTask;
