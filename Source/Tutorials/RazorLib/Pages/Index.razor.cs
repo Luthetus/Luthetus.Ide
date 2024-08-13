704b3458b7a127edb304d90fddb6d5691d35264a
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-08-13 13:44:56 -0400

    text editor appears

--- Source/Tutorials/RazorLib/Pages/Index.razor.cs
+++ Source/Tutorials/RazorLib/Pages/Index.razor.cs
@@ -1,7 +1,38 @@
 using Microsoft.AspNetCore.Components;
+using Luthetus.Common.RazorLib.Keys.Models;
+using Luthetus.TextEditor.RazorLib;
+using Luthetus.TextEditor.RazorLib.TextEditors.Models;
+using Luthetus.TextEditor.RazorLib.Lexers.Models;
 
 namespace Luthetus.Tutorials.RazorLib.Pages;
 
 public partial class Index : ComponentBase
 {
+	[Inject]
+	private ITextEditorService TextEditorService { get; set; } = null!;
+
+	public static ResourceUri ResourceUri { get; } = new("/index.txt");
+	public static Key<TextEditorViewModel> ViewModelKey { get; } = Key<TextEditorViewModel>.NewKey();
+	
+	protected override void OnInitialized()
+	{
+		var existingModel = TextEditorService.ModelApi.GetOrDefault(ResourceUri);
+		if (existingModel is not null)
+			return;
+	
+		var model = new TextEditorModel(
+	        ResourceUri,
+	        DateTime.UtcNow,
+	        ExtensionNoPeriodFacts.TXT,
+	        "abc123",
+	        decorationMapper: null,
+	        compilerService: null);
+	
+		TextEditorService.ModelApi.RegisterCustom(model);
+		
+		TextEditorService.ViewModelApi.Register(
+			ViewModelKey,
+			ResourceUri,
+			new Category("main"));
+	}
 }
