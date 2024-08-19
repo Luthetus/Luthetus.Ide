0101951228d7718c3a5924853f7fa24afb1a2e37
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-08-19 12:34:41 -0400

    Start: CommandBarDisplay.razor

--- Source/Lib/Ide/Ide.RazorLib/CommandBars/Displays/CommandBarDisplay.razor.cs
+++ Source/Lib/Ide/Ide.RazorLib/CommandBars/Displays/CommandBarDisplay.razor.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
 using Fluxor;
 using Fluxor.Blazor.Web.Components;
+using Luthetus.Common.RazorLib.JsRuntimes.Models;
 using Luthetus.Ide.RazorLib.CommandBars.States;
 
 namespace Luthetus.Ide.RazorLib.CommandBars.Displays;
@@ -12,6 +14,35 @@ public partial class CommandBarDisplay : FluxorComponent
 	private IState<CommandBarState> CommandBarStateWrap { get; set; } = null!;
 	[Inject]
 	private IDispatcher Dispatcher { get; set; } = null!;
+	[Inject]
+	private IJSRuntime JsRuntime { get; set; } = null!;
+	
+	public const string INPUT_HTML_ELEMENT_ID = "luth_ide_command-bar-input-id";
+	
+	private bool _previousShouldDisplay = false;
+	private LuthetusCommonJavaScriptInteropApi _jsRuntimeCommonApi;
+	
+	private LuthetusCommonJavaScriptInteropApi JsRuntimeCommonApi => _jsRuntimeCommonApi
+		??= JsRuntime.GetLuthetusCommonApi();
+	
+	protected override async Task OnAfterRenderAsync(bool firstRender)
+	{
+		var shouldDisplay = CommandBarStateWrap.Value.ShouldDisplay;
+		
+		if (_previousShouldDisplay != shouldDisplay)
+		{
+			_previousShouldDisplay = shouldDisplay;
+			
+			if (shouldDisplay)
+			{
+				await JsRuntimeCommonApi
+					.FocusHtmlElementById(CommandBarDisplay.INPUT_HTML_ELEMENT_ID)
+	                .ConfigureAwait(false);
+			}
+		}
+        			
+		await base.OnAfterRenderAsync(firstRender);
+	}
 	
 	private void HandleOnKeyDown(KeyboardEventArgs keyboardEventArgs)
 	{
@@ -23,5 +54,11 @@ public partial class CommandBarDisplay : FluxorComponent
 		{
 			Dispatcher.Dispatch(new CommandBarState.SetShouldDisplayAction(false));
 		}
+	}
+	
+	private Task OnOutOfBoundsClick()
+	{
+		Dispatcher.Dispatch(new CommandBarState.SetShouldDisplayAction(false));
+		return Task.CompletedTask;
 	}
 }
