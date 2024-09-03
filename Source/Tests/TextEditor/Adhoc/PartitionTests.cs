1e27e2ce3da945d32edf2eaf025a2c714848a567
Hunter Freeman <luthetus@gmail.com>
2024-09-03 10:48:13 -0400

    Aaa

---
+++ Source/Tests/TextEditor/Adhoc/PartitionTests.cs
@@ -0,0 +1,21 @@
+namespace Luthetus.TextEditor.Tests.Adhoc;
+
+public class PartitionTests
+{
+	/// <summary>
+	/// When a deletion of text spans multiple partitions it seems to sometimes break the line endings.
+	///
+	/// Who do I name things 'Aaa' on occassion?
+	///
+	/// "What Do You Do When You Can't Do Anything"
+	/// https://youtu.be/JRqB2pm33IM?si=UhQxGhT8HcETN4xa
+	///
+	/// By using the name 'Aaa' consistently in this situations, I can easily find these naming scenarios
+	/// and fix them when there is a better mood.
+	/// </summary>
+	[Fact]
+	public void Aaa()
+	{
+		throw new NotImplementedException();
+	}
+}
