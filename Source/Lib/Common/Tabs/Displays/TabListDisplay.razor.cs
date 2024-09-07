21bf5f6a16be71504b3ddc4c5a99f1d3e83728c5
Hunter Freeman <luthetus@gmail.com>
2024-09-07 15:00:24 -0400

    Remove failed fixes

--- Source/Lib/Common/Tabs/Displays/TabListDisplay.razor.cs
+++ Source/Lib/Common/Tabs/Displays/TabListDisplay.razor.cs
@@ -10,7 +10,7 @@ using Luthetus.Common.RazorLib.Notifications.Models;
 
 namespace Luthetus.Common.RazorLib.Tabs.Displays;
 
-public partial class TabListDisplay : ComponentBase, IDisposable
+public partial class TabListDisplay : ComponentBase
 {
 	[Inject]
 	private ICommonComponentRenderers CommonComponentRenderers { get; set; } = null!;
@@ -23,141 +23,9 @@ public partial class TabListDisplay : ComponentBase, IDisposable
 	[Parameter]
 	public string CssClassString { get; set; } = string.Empty;
 	
-	private readonly object _disposeLock = new();
-	
-	private bool _disposed;
-
     public async Task NotifyStateChangedAsync()
 	{
-		// This method has a race condition when one drags a tab
-		// off the text editor group tab listing.
-		//
-		// Exception text:
-		// =============================
-		// (Luthetus.Ide.Photino:28433): Gtk-WARNING **: 13:30:13.496: Failed to set text 'System.NullReferenceException: Object reference not set to an instance of an object.
-		//   at Luthetus.Common.RazorLib.Tabs.Displays.TabDisplay.BuildRenderTree(RenderTreeBuilder __builder) in /home/hunter/Repos/Luthetus.Ide_Fork/Source/Lib/Common/Tabs/Displays/TabDisplay.razor:line 1
-		//   at Microsoft.AspNetCore.Components.Rendering.ComponentState.RenderIntoBatch(RenderBatchBuilder batchBuilder, RenderFragment renderFragment, Exception& renderFragmentException)
-		//--- End of stack trace from previous location ---
-		//   at Microsoft.AspNetCore.Components.WebView.IpcSender.<>c__DisplayClass11_0.<NotifyUnhandledException>b__1()
-		//   at Photino.Blazor.PhotinoDispatcher.InvokeAsync(Action workItem)
-		//   at Microsoft.AspNetCore.Components.WebView.IpcSender.NotifyUnhandledException(Exception exception)
-		//   at Microsoft.AspNetCore.Components.WebView.Services.WebViewRenderer.HandleException(Exception exception)
-		//   at Microsoft.AspNetCore.Components.RenderTree.Renderer.HandleExceptionViaErrorBoundary(Exception error, ComponentState errorSourceOrNull)
-		//   at Microsoft.AspNetCore.Components.RenderTree.Renderer.ProcessRenderQueue()
-		//--- End of stack trace from previous location ---
-		//   at Microsoft.AspNetCore.Components.WebView.IpcSender.<>c__DisplayClass11_0.<NotifyUnhandledException>b__1()
-		//   at Photino.Blazor.PhotinoDispatcher.InvokeAsync(Action workItem)
-		//   at Microsoft.AspNetCore.Components.WebView.IpcSender.NotifyUnhandledException(Exception exception)
-		//   at Microsoft.AspNetCore.Components.WebView.Services.WebViewRenderer.HandleException(Exception exception)
-		//   at Microsoft.AspNetCore.Components.RenderTree.Renderer.ProcessRenderQueue()
-		//   at Microsoft.AspNetCore.Components.ComponentBase.StateHasChanged()
-		//   at Photino.Blazor.PhotinoSynchronizationContext.<>c.<InvokeAsync>b__11_0(Object state)
-		//--- End of stack trace from previous location ---
-		//   at Luthetus.Ide.RazorLib.Shareds.Displays.IdeMainLayout.DragStateWrapOnStateChanged(Object sender, EventArgs e) in /home/hunter/Repos/Luthetus.Ide_Fork/Source/Lib/Ide/Ide.RazorLib/Shareds/Displays/IdeMainLayout.razor.cs:line 111
-		//   at System.Threading.Tasks.Task.<>c.<ThrowAsync>b__128_1(Object state)
-		//   at System.Threading.ThreadPoolWorkQueue.Dispatch()
-		//   at System.Threading.PortableThreadPool.WorkerThread.WorkerThreadStart()' from markup due to error parsing markup: Error on line 3: Entity did not end with a semicolon; most likely you used an ampersand character without intending to start an entity — escape ampersand as &amp;
-		//
-		//(Luthetus.Ide.Photino:28433): Gtk-WARNING **: 13:30:13.511: Failed to set text 'System.NullReferenceException: Object reference not set to an instance of an object.
-		//   at Luthetus.Common.RazorLib.Tabs.Displays.TabDisplay.BuildRenderTree(RenderTreeBuilder __builder) in /home/hunter/Repos/Luthetus.Ide_Fork/Source/Lib/Common/Tabs/Displays/TabDisplay.razor:line 1
-		//   at Microsoft.AspNetCore.Components.Rendering.ComponentState.RenderIntoBatch(RenderBatchBuilder batchBuilder, RenderFragment renderFragment, Exception& renderFragmentException)
-		//--- End of stack trace from previous location ---
-		//   at Microsoft.AspNetCore.Components.WebView.IpcSender.<>c__DisplayClass11_0.<NotifyUnhandledException>b__1()
-		//   at Photino.Blazor.PhotinoDispatcher.InvokeAsync(Action workItem)
-		//   at Microsoft.AspNetCore.Components.WebView.IpcSender.NotifyUnhandledException(Exception exception)
-		//   at Microsoft.AspNetCore.Components.WebView.Services.WebViewRenderer.HandleException(Exception exception)
-		//   at Microsoft.AspNetCore.Components.RenderTree.Renderer.HandleExceptionViaErrorBoundary(Exception error, ComponentState errorSourceOrNull)
-		//   at Microsoft.AspNetCore.Components.RenderTree.Renderer.ProcessRenderQueue()
-		//--- End of stack trace from previous location ---
-		//   at Microsoft.AspNetCore.Components.WebView.IpcSender.<>c__DisplayClass11_0.<NotifyUnhandledException>b__1()
-		//   at Photino.Blazor.PhotinoDispatcher.InvokeAsync(Action workItem)
-		//   at Microsoft.AspNetCore.Components.WebView.IpcSender.NotifyUnhandledException(Exception exception)
-		//   at Microsoft.AspNetCore.Components.WebView.Services.WebViewRenderer.HandleException(Exception exception)
-		//   at Microsoft.AspNetCore.Components.RenderTree.Renderer.ProcessRenderQueue()
-		//   at Microsoft.AspNetCore.Components.ComponentBase.StateHasChanged()
-		//   at Photino.Blazor.PhotinoSynchronizationContext.<>c.<InvokeAsync>b__11_0(Object state)
-		//--- End of stack trace from previous location ---
-		//   at Luthetus.Ide.RazorLib.Shareds.Displays.IdeMainLayout.DragStateWrapOnStateChanged(Object sender, EventArgs e) in /home/hunter/Repos/Luthetus.Ide_Fork/Source/Lib/Ide/Ide.RazorLib/Shareds/Displays/IdeMainLayout.razor.cs:line 111
-		//   at System.Threading.Tasks.Task.<>c.<ThrowAsync>b__128_1(Object state)
-		//   at System.Threading.ThreadPoolWorkQueue.Dispatch()
-		//   at System.Threading.PortableThreadPool.WorkerThread.WorkerThreadStart()' from markup due to error parsing markup: Error on line 3: Entity did not end with a semicolon; most likely you used an ampersand character without intending to start an entity — escape ampersand as &amp;
-		//Unhandled exception. System.NullReferenceException: Object reference not set to an instance of an object.
-		//   at Luthetus.Common.RazorLib.Tabs.Displays.TabDisplay.BuildRenderTree(RenderTreeBuilder __builder) in /home/hunter/Repos/Luthetus.Ide_Fork/Source/Lib/Common/Tabs/Displays/TabDisplay.razor:line 1
-		//   at Microsoft.AspNetCore.Components.Rendering.ComponentState.RenderIntoBatch(RenderBatchBuilder batchBuilder, RenderFragment renderFragment, Exception& renderFragmentException)
-		//--- End of stack trace from previous location ---
-		//   at Microsoft.AspNetCore.Components.WebView.IpcSender.<>c__DisplayClass11_0.<NotifyUnhandledException>b__1()
-		//   at Photino.Blazor.PhotinoDispatcher.InvokeAsync(Action workItem)
-		//   at Microsoft.AspNetCore.Components.WebView.IpcSender.NotifyUnhandledException(Exception exception)
-		//   at Microsoft.AspNetCore.Components.WebView.Services.WebViewRenderer.HandleException(Exception exception)
-		//   at Microsoft.AspNetCore.Components.RenderTree.Renderer.HandleExceptionViaErrorBoundary(Exception error, ComponentState errorSourceOrNull)
-		//   at Microsoft.AspNetCore.Components.RenderTree.Renderer.ProcessRenderQueue()
-		//--- End of stack trace from previous location ---
-		//   at Microsoft.AspNetCore.Components.WebView.IpcSender.<>c__DisplayClass11_0.<NotifyUnhandledException>b__1()
-		//   at Photino.Blazor.PhotinoDispatcher.InvokeAsync(Action workItem)
-		//   at Microsoft.AspNetCore.Components.WebView.IpcSender.NotifyUnhandledException(Exception exception)
-		//   at Microsoft.AspNetCore.Components.WebView.Services.WebViewRenderer.HandleException(Exception exception)
-		//   at Microsoft.AspNetCore.Components.RenderTree.Renderer.ProcessRenderQueue()
-		//   at Microsoft.AspNetCore.Components.ComponentBase.StateHasChanged()
-		//   at Photino.Blazor.PhotinoSynchronizationContext.<>c.<InvokeAsync>b__11_0(Object state)
-		//--- End of stack trace from previous location ---
-		//   at Luthetus.Ide.RazorLib.Shareds.Displays.IdeMainLayout.DragStateWrapOnStateChanged(Object sender, EventArgs e) in /home/hunter/Repos/Luthetus.Ide_Fork/Source/Lib/Ide/Ide.RazorLib/Shareds/Displays/IdeMainLayout.razor.cs:line 111
-		//   at System.Threading.Tasks.Task.<>c.<ThrowAsync>b__128_1(Object state)
-		//   at System.Threading.ThreadPoolWorkQueue.Dispatch()
-		//   at System.Threading.PortableThreadPool.WorkerThread.WorkerThreadStart()
-		//
-		// More Information
-		// ================
-		// This try catch didn't fix the issue. It seems the .razor is crashing.
-		// Presumably something to do with .razor exceptions being fatal exceptions through way of middleware?
-		//
-		// I'm considering doing something with IDisposable, but it sounds like a nightmare scenario
-		// to try and add concurrency safety here.
-		//
-		// The tab is perhaps no longer rendered as it gets told via this method to render?
-		//
-		// Maybe the underlying ComponentBase RenderHandle, or such, logic was disposed/null'd out
-		// but this object reference was still available.
-		await InvokeAsync(() =>
-		{
-			try
-			{
-				lock (_disposeLock)
-		    	{
-		    		if (!_disposed)
-		    		{
-		    			StateHasChanged();
-		    		}
-		    		else
-		    		{
-			    		var title = $"{nameof(TabListDisplay)} race condition occurred.\n";
-						var message = "the issue was prevented";
-					
-						NotificationHelper.DispatchError(
-					        title,
-					        message,
-					        CommonComponentRenderers,
-					        Dispatcher,
-					        TimeSpan.FromSeconds(6));
-					
-						Console.WriteLine(title + message);
-		    		}
-		    	}
-		    }
-		    catch (Exception e)
-			{
-				var title = $"{nameof(TabListDisplay)} race condition occurred.\n";
-				var message = e.ToString();
-			
-				NotificationHelper.DispatchError(
-			        title,
-			        message,
-			        CommonComponentRenderers,
-			        Dispatcher,
-			        TimeSpan.FromSeconds(6));
-			
-				Console.WriteLine(title + message);
-			}
-		});
+		await InvokeAsync(StateHasChanged);
 	}
 
 	private Task HandleTabButtonOnContextMenu(TabContextMenuEventArgs tabContextMenuEventArgs)
@@ -178,13 +46,5 @@ public partial class TabListDisplay : ComponentBase, IDisposable
 
         Dispatcher.Dispatch(new DropdownState.RegisterAction(dropdownRecord));
         return Task.CompletedTask;
-    }
-    
-    public void Dispose()
-    {
-    	lock (_disposeLock)
-    	{
-    		_disposed = true;
-    	}
     }
 }
