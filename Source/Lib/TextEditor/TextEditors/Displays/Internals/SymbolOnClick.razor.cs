42461ddcfb2acfde7487dfba9995c94688707b8b
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-11-19 19:46:14 -0500

    Tooltip onclick goes to the reference of the type but should be the definition in the future

--- Source/Lib/TextEditor/TextEditors/Displays/Internals/SymbolOnClick.razor.cs
+++ Source/Lib/TextEditor/TextEditors/Displays/Internals/SymbolOnClick.razor.cs
@@ -1,15 +1,26 @@
 using Microsoft.AspNetCore.Components;
+using Luthetus.Common.RazorLib.Keys.Models;
+using Luthetus.TextEditor.RazorLib;
 using Luthetus.TextEditor.RazorLib.Lexers.Models;
+using Luthetus.TextEditor.RazorLib.TextEditors.Models;
 
 namespace Luthetus.TextEditor.RazorLib.TextEditors.Displays.Internals;
 
 public partial class SymbolOnClick : ComponentBase
 {
+	[Inject]
+	private ITextEditorService TextEditorService { get; set; } = null!;
+
 	[Parameter, EditorRequired]
 	public (string ClassCssString, string Text, TextEditorTextSpan TextSpan) Parameter  { get; set; }
 	
-	private void HandleOnClick()
+	private Task HandleOnClick()
 	{
-		
+		return TextEditorService.OpenInEditorAsync(
+			Parameter.TextSpan.ResourceUri.Value,
+			true,
+			Parameter.TextSpan.StartingIndexInclusive,
+			new Category("main"),
+			Key<TextEditorViewModel>.NewKey());
 	}
 }
