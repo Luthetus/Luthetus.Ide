c5363aa9d7de49c72e1bf6e8ead9230f102435b1
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-11-21 18:26:41 -0500

    Start by making common variables

--- Source/Lib/TextEditor/TextEditors/Displays/Internals/Symbols/TypeSyntaxDisplay.razor.cs
+++ Source/Lib/TextEditor/TextEditors/Displays/Internals/Symbols/TypeSyntaxDisplay.razor.cs
@@ -4,6 +4,9 @@ using Luthetus.TextEditor.RazorLib;
 using Luthetus.TextEditor.RazorLib.TextEditors.Models;
 using Luthetus.TextEditor.RazorLib.TextEditors.Models.Internals;
 using Luthetus.TextEditor.RazorLib.Lexers.Models;
+using Luthetus.TextEditor.RazorLib.CompilerServices.Syntax;
+using Luthetus.TextEditor.RazorLib.CompilerServices.Syntax.Nodes;
+using Luthetus.TextEditor.RazorLib.CompilerServices.Syntax.Symbols;
 
 namespace Luthetus.TextEditor.RazorLib.TextEditors.Displays.Internals.Symbols;
 
@@ -14,16 +17,21 @@ public partial class TypeSyntaxDisplay : ComponentBase
 	
 	[Parameter, EditorRequired]
 	public SyntaxViewModel SyntaxViewModel { get; set; } = default!;
-
-	[Parameter, EditorRequired]
-	public (string ClassCssString, string Text, TextEditorTextSpan TextSpan) Parameter { get; set; }
 	
 	private Task HandleOnClick(SyntaxViewModel syntaxViewModelLocal)
 	{
+		if (syntaxViewModelLocal.DefinitionNode is null ||
+			syntaxViewModelLocal.DefinitionNode.SyntaxKind != SyntaxKind.TypeDefinitionNode)
+		{
+			return Task.CompletedTask;
+		}
+		
+		var typeDefinitionNode = (TypeDefinitionNode)syntaxViewModelLocal.DefinitionNode;
+	
 		return TextEditorService.OpenInEditorAsync(
-			Parameter.TextSpan.ResourceUri.Value,
+			typeDefinitionNode.TypeIdentifierToken.TextSpan.ResourceUri.Value,
 			true,
-			Parameter.TextSpan.StartingIndexInclusive,
+			typeDefinitionNode.TypeIdentifierToken.TextSpan.StartingIndexInclusive,
 			new Category("main"),
 			Key<TextEditorViewModel>.NewKey());
 	}
