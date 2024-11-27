25c369d1ee25d6d605ee2c530c4170ada31de41a
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-11-27 12:57:54 -0500

    SyntaxViewModel.Depth

--- Source/Lib/TextEditor/TextEditors/Models/Internals/SyntaxViewModel.cs
+++ Source/Lib/TextEditor/TextEditors/Models/Internals/SyntaxViewModel.cs
@@ -28,16 +28,60 @@ namespace Luthetus.TextEditor.RazorLib.TextEditors.Models.Internals;
 /// </summary>
 public struct SyntaxViewModel
 {
-	public SyntaxViewModel(ITextEditorSymbol? symbol, ISyntaxNode? targetNode, ISyntaxNode? definitionNode)
+	public SyntaxViewModel(
+		ITextEditorSymbol? targetSymbol,
+		ISyntaxNode? targetNode,
+		ISyntaxNode? definitionNode,
+		int depth)
 	{
-		Symbol = symbol;
+		TargetSymbol = targetSymbol;
 		TargetNode = targetNode;
 		DefinitionNode = definitionNode;
+		Depth = depth;
 	}
 	
-	public ITextEditorSymbol? Symbol { get; }
+	/// <summary>
+	/// The user interface gesture is to hover a symbol in the text editor.
+	/// Other than a UI event, it is unlikely this property is non-null.
+	/// </summary>
+	public ITextEditorSymbol? TargetSymbol { get; }
+	
+	/// <summary>
+	/// The user interface provided an 'ITextEditorSymbol',
+	/// but there is little data available on a symbol relative
+	/// to the an ISyntaxNode it represents.
+	///
+	/// Therefore, the symbol is mapped to its corresponding node,
+	/// so that more information can be displayed on the UI.
+	/// </summary>
 	public ISyntaxNode? TargetNode { get; }
+	
+	/// <summary>
+	/// The user interface goes from an ISymbol to an ISyntaxNode,
+	/// but that ISyntaxNode might be a reference to a definition node, declaration node, or etc...
+	///
+	/// So, since there is other data that exists on the definition node, that the reference node
+	/// does not have access to, then the definition node is retrieved
+	/// in order to display more information on the UI.
+	///
+	/// As well, if there is a definition node, then when
+	/// the text is hovered, it should show an underline and be clickable.
+	/// This click will then 'goto definition' to that node.
+	/// </summary>
 	public ISyntaxNode? DefinitionNode { get; }
+	
+	/// <summary>
+	/// The 'TypeSyntaxDisplay' is used recursively because a 'TypeClauseNode'
+	/// can have a 'GenericParametersListingNode' which goes on to contains other 'TypeClauseNode'
+	/// (which as well may or may not have their own 'GenericParametersListingNode').
+	///
+	/// But, it is only for a 'Depth == 0' that extra information about the
+	/// node should be added to the UI. (is it public..., partial, class or struct..., etc...)
+	/// </summary>
+	public int Depth { get; }
+	
+	public bool ShowPrefixText { get; } = false;
+	public bool ShouldTryResolveNullDefinitionNode { get; } = false;
 	
 	public Task HandleOnClick(ITextEditorService textEditorService, SyntaxKind syntaxKindExpected)
 	{
