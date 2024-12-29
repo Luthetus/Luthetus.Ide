e85d0b148529e15181a049560322b948e47a2b38
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2024-12-29 12:00:57 -0500

    Delete VirtualizationRequest.cs

--- Source/Lib/TextEditor/Virtualizations/Models/VirtualizationRequest.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Luthetus.TextEditor.RazorLib.Virtualizations.Models;
-
-public record VirtualizationRequest(
-    VirtualizationScrollPosition ScrollPosition,
-    CancellationToken CancellationToken);
