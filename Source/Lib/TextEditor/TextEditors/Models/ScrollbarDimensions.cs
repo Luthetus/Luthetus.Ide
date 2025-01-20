c286262dee68d7c459cf80432844af02faa5a470
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-01-20 13:58:25 -0500

    ScrollBarDimensions and TextEditorDimensions changed to 'record struct'

--- Source/Lib/TextEditor/TextEditors/Models/ScrollbarDimensions.cs
+++ Source/Lib/TextEditor/TextEditors/Models/ScrollbarDimensions.cs
@@ -9,7 +9,7 @@ namespace Luthetus.TextEditor.RazorLib.TextEditors.Models;
 /// <param name="ScrollWidth">The unit of measurement is Pixels (px)</param>
 /// <param name="ScrollHeight">The unit of measurement is Pixels (px)</param>
 /// <param name="MarginScrollHeight">The unit of measurement is Pixels (px)</param>
-public sealed record ScrollbarDimensions(
+public record struct ScrollbarDimensions(
 	double ScrollLeft,
     double ScrollTop,
     double ScrollWidth,
