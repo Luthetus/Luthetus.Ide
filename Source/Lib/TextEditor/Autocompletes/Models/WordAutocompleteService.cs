64033c6bd1ea734c3df186af7ad617a3364810e7
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-01-21 21:27:44 -0500

    (2025-01-21)

--- Source/Lib/TextEditor/Autocompletes/Models/WordAutocompleteService.cs
+++ Source/Lib/TextEditor/Autocompletes/Models/WordAutocompleteService.cs
@@ -1,4 +1,4 @@
-﻿namespace Luthetus.TextEditor.RazorLib.Autocompletes.Models;
+namespace Luthetus.TextEditor.RazorLib.Autocompletes.Models;
 
 public class WordAutocompleteService : IAutocompleteService
 {
@@ -11,6 +11,17 @@ public class WordAutocompleteService : IAutocompleteService
 
     public List<string> GetAutocompleteOptions(string word)
     {
+    	// (2025-01-21)
+    	// ==============================================
+    	// Value does not fall within the expected range.
+		// 	at System.Collections.Immutable.ImmutableExtensions.ToArray[T](IEnumerable`1 sequence, Int32 count)
+		// 	at System.Collections.Immutable.ImmutableArray.CreateRange[T](IEnumerable`1 items)
+		// 	at Luthetus.TextEditor.RazorLib.Autocompletes.Models.WordAutocompleteService.GetAutocompleteOptions(String word) in C:\Users\hunte\Repos\Luthetus.Ide_Fork\Source\Lib\TextEditor\Autocompletes\Models\WordAutocompleteService.cs:line 14
+		// 	at Luthetus.TextEditor.RazorLib.TextEditors.Displays.Internals.AutocompleteMenu.GetMenuRecord()
+		// 	at Luthetus.TextEditor.RazorLib.TextEditors.Displays.Internals.AutocompleteMenu.<>c__DisplayClass33_0.<BuildRenderTree>b__1(RenderTreeBuilder __builder2)
+		// 	at Microsoft.AspNetCore.Components.CascadingValue`1.Render(RenderTreeBuilder builder)
+		// 	at Microsoft.AspNetCore.Components.Rendering.ComponentState.RenderIntoBatch(RenderBatchBuilder batchBuilder, RenderFragment renderFragment, Exception& renderFragmentException)
+    	
         var indexedStrings = _wordAutocompleteIndexer.IndexedStringsList;
         return new List<string>(indexedStrings.Where(x => x.StartsWith(word)).Take(5));
     }
