759ebf29e6db9de133f7c9444aa9834344de37b5
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-01-29 18:12:18 -0500

    Inline: PresentationAndSelectionDriver.cs

--- Source/Lib/TextEditor/TextEditors/Displays/TextEditorViewModelDisplay.razor.cs
+++ Source/Lib/TextEditor/TextEditors/Displays/TextEditorViewModelDisplay.razor.cs
@@ -57,6 +57,18 @@ using Luthetus.Common.RazorLib.Dimensions.Models;
 using Luthetus.TextEditor.RazorLib.TextEditors.Models.Internals;
 using Luthetus.TextEditor.RazorLib.Virtualizations.Models;
 
+// PresentationAndSelectionDriver.cs
+using System.Collections.Immutable;
+using Microsoft.AspNetCore.Components;
+using Luthetus.Common.RazorLib.Keys.Models;
+using Luthetus.Common.RazorLib.Dimensions.Models;
+using Luthetus.TextEditor.RazorLib.Decorations.Models;
+using Luthetus.TextEditor.RazorLib.Cursors.Models;
+using Luthetus.TextEditor.RazorLib.Lexers.Models;
+using Luthetus.TextEditor.RazorLib.Exceptions;
+using Luthetus.TextEditor.RazorLib.TextEditors.Models;
+using Luthetus.TextEditor.RazorLib.TextEditors.Models.Internals;
+
 namespace Luthetus.TextEditor.RazorLib.TextEditors.Displays;
 
 public sealed partial class TextEditorViewModelDisplay : ComponentBase, IDisposable
@@ -117,9 +129,6 @@ public sealed partial class TextEditorViewModelDisplay : ComponentBase, IDisposa
     
     private TextEditorViewModel? _linkedViewModel;
     
-    // TODO: awkward public
-    public PresentationAndSelectionDriver _presentationAndSelectionDriver;
-    
     private Dictionary<string, object?> DependentComponentParameters;
     
     private bool _thinksTouchIsOccurring;
@@ -158,8 +167,6 @@ public sealed partial class TextEditorViewModelDisplay : ComponentBase, IDisposa
     		}
     	};
 
-    	_presentationAndSelectionDriver = new(this);
-    
         ConstructRenderBatch();
 
 		SetComponentData();
@@ -1473,6 +1480,373 @@ public sealed partial class TextEditorViewModelDisplay : ComponentBase, IDisposa
     }
 
     #endregion ScrollbarSectionClose
+    
+    #region PresentationAndSelectionDriverOpen
+    
+    public List<TextEditorPresentationModel> GetTextEditorPresentationModels(
+    	TextEditorRenderBatch renderBatchLocal,
+    	IReadOnlyList<Key<TextEditorPresentationModel>> textEditorPresentationKeys)
+    {
+    	var textEditorPresentationModelList = new List<TextEditorPresentationModel>();
+
+        foreach (var presentationKey in textEditorPresentationKeys)
+        {
+            var textEditorPresentationModel = renderBatchLocal.Model.PresentationModelList.FirstOrDefault(x =>
+                x.TextEditorPresentationKey == presentationKey);
+
+            if (textEditorPresentationModel is not null)
+                textEditorPresentationModelList.Add(textEditorPresentationModel);
+        }
+
+        return textEditorPresentationModelList;
+    }
+
+    public string PresentationGetCssStyleString(
+    	TextEditorRenderBatch renderBatchLocal,
+        int lowerPositionIndexInclusive,
+        int upperPositionIndexExclusive,
+        int rowIndex)
+    {
+    	try
+        {
+            var charMeasurements = renderBatchLocal.ViewModel.CharAndLineMeasurements;
+			var textEditorDimensions = renderBatchLocal.ViewModel.TextEditorDimensions;
+            var scrollbarDimensions = renderBatchLocal.ViewModel.ScrollbarDimensions;
+
+            if (rowIndex >= renderBatchLocal.Model.LineEndList.Count)
+                return string.Empty;
+
+            var line = renderBatchLocal.Model.GetLineInformation(rowIndex);
+
+            var startingColumnIndex = 0;
+            var endingColumnIndex = line.EndPositionIndexExclusive - 1;
+
+            var fullWidthOfRowIsSelected = true;
+
+            if (lowerPositionIndexInclusive > line.StartPositionIndexInclusive)
+            {
+                startingColumnIndex = lowerPositionIndexInclusive - line.StartPositionIndexInclusive;
+                fullWidthOfRowIsSelected = false;
+            }
+
+            if (upperPositionIndexExclusive < line.EndPositionIndexExclusive)
+            {
+                endingColumnIndex = upperPositionIndexExclusive - line.StartPositionIndexInclusive;
+                fullWidthOfRowIsSelected = false;
+            }
+
+            var topInPixelsInvariantCulture = (rowIndex * charMeasurements.LineHeight).ToCssValue();
+
+            var top = $"top: {topInPixelsInvariantCulture}px;";
+
+            var heightInPixelsInvariantCulture = charMeasurements.LineHeight.ToCssValue();
+
+            var height = $"height: {heightInPixelsInvariantCulture}px;";
+
+            var startInPixels = startingColumnIndex * charMeasurements.CharacterWidth;
+
+            // startInPixels offset from Tab keys a width of many characters
+            {
+                var tabsOnSameRowBeforeCursor = renderBatchLocal.Model.GetTabCountOnSameLineBeforeCursor(
+                    rowIndex,
+                    startingColumnIndex);
+
+                // 1 of the character width is already accounted for
+                var extraWidthPerTabKey = TextEditorModel.TAB_WIDTH - 1;
+
+                startInPixels += extraWidthPerTabKey * tabsOnSameRowBeforeCursor * charMeasurements.CharacterWidth;
+            }
+
+            var startInPixelsInvariantCulture = startInPixels.ToCssValue();
+            var left = $"left: {startInPixelsInvariantCulture}px;";
+
+            var widthInPixels = endingColumnIndex * charMeasurements.CharacterWidth - startInPixels;
+
+            // Tab keys a width of many characters
+            {
+                var tabsOnSameRowBeforeCursor = renderBatchLocal.Model.GetTabCountOnSameLineBeforeCursor(
+                    rowIndex,
+                    line.LastValidColumnIndex);
+
+                // 1 of the character width is already accounted for
+                var extraWidthPerTabKey = TextEditorModel.TAB_WIDTH - 1;
+
+                widthInPixels += extraWidthPerTabKey * tabsOnSameRowBeforeCursor * charMeasurements.CharacterWidth;
+            }
+
+            var widthCssStyleString = "width: ";
+
+            var fullWidthValue = scrollbarDimensions.ScrollWidth;
+
+            if (textEditorDimensions.Width > scrollbarDimensions.ScrollWidth)
+                fullWidthValue = textEditorDimensions.Width; // If content does not fill the viewable width of the Text Editor User Interface
+
+            var fullWidthValueInPixelsInvariantCulture = fullWidthValue.ToCssValue();
+
+            var widthInPixelsInvariantCulture = widthInPixels.ToCssValue();
+
+            if (fullWidthOfRowIsSelected)
+                widthCssStyleString += $"{fullWidthValueInPixelsInvariantCulture}px;";
+            else if (startingColumnIndex != 0 && upperPositionIndexExclusive > line.EndPositionIndexExclusive - 1)
+                widthCssStyleString += $"calc({fullWidthValueInPixelsInvariantCulture}px - {startInPixelsInvariantCulture}px);";
+            else
+                widthCssStyleString += $"{widthInPixelsInvariantCulture}px;";
+
+            return $"position: absolute; {top} {height} {left} {widthCssStyleString}";
+        }
+        catch (LuthetusTextEditorException)
+        {
+            return string.Empty;
+        }
+    }
+
+    public string PresentationGetCssClass(TextEditorPresentationModel presentationModel, byte decorationByte)
+    {
+        return presentationModel.DecorationMapper.Map(decorationByte);
+    }
+
+    public IReadOnlyList<TextEditorTextSpan> PresentationVirtualizeAndShiftTextSpans(
+    	TextEditorRenderBatch renderBatchLocal,
+        IReadOnlyList<TextEditorTextModification> textModifications,
+        IReadOnlyList<TextEditorTextSpan> inTextSpanList)
+    {
+    	// (2025-01-22)
+    	// ============
+    	// The text spans need to be tied to the partitions they reside in
+    	// (careful of a textspan that overlaps two partitions).
+    	//
+    	// This shouldn't be done here, it should be done during the editContext.
+    	
+    	try
+        {
+            // Virtualize the text spans
+            var virtualizedTextSpanList = new List<TextEditorTextSpan>();
+            if (renderBatchLocal.ViewModel.VirtualizationResult.EntryList.Any())
+            {
+                var lowerLineIndexInclusive = renderBatchLocal.ViewModel.VirtualizationResult.EntryList.First().LineIndex;
+                var upperLineIndexInclusive = renderBatchLocal.ViewModel.VirtualizationResult.EntryList.Last().LineIndex;
+
+                var lowerLine = renderBatchLocal.Model.GetLineInformation(lowerLineIndexInclusive);
+                var upperLine = renderBatchLocal.Model.GetLineInformation(upperLineIndexInclusive);
+
+				// Awkward enumeration was modified 'for loop' (2025-01-22)
+				// Also, this shouldn't be done here, it should be done during the editContext.
+				var count = inTextSpanList.Count;
+                for (int i = 0; i < count; i++)
+                {
+                	var textSpan = inTextSpanList[i];
+                	
+                    if (lowerLine.StartPositionIndexInclusive <= textSpan.StartingIndexInclusive &&
+                        upperLine.EndPositionIndexExclusive >= textSpan.StartingIndexInclusive)
+                    {
+                        virtualizedTextSpanList.Add(textSpan);
+                    }
+                }
+            }
+            else
+            {
+                // No 'VirtualizationResult', so don't render any text spans.
+                return Array.Empty<TextEditorTextSpan>();
+            }
+
+            var outTextSpansList = new List<TextEditorTextSpan>();
+            // Shift the text spans
+            {
+                foreach (var textSpan in virtualizedTextSpanList)
+                {
+                    var startingIndexInclusive = textSpan.StartingIndexInclusive;
+                    var endingIndexExclusive = textSpan.EndingIndexExclusive;
+
+					// Awkward enumeration was modified 'for loop' (2025-01-22)
+					// Also, this shouldn't be done here, it should be done during the editContext.
+					var count = textModifications.Count;
+                    for (int i = 0; i < count; i++)
+                    {
+                    	var textModification = textModifications[i];
+                    
+                        if (textModification.WasInsertion)
+                        {
+                            if (startingIndexInclusive >= textModification.TextEditorTextSpan.StartingIndexInclusive)
+                            {
+                                startingIndexInclusive += textModification.TextEditorTextSpan.Length;
+                                endingIndexExclusive += textModification.TextEditorTextSpan.Length;
+                            }
+                        }
+                        else // was deletion
+                        {
+                            if (startingIndexInclusive >= textModification.TextEditorTextSpan.StartingIndexInclusive)
+                            {
+                                startingIndexInclusive -= textModification.TextEditorTextSpan.Length;
+                                endingIndexExclusive -= textModification.TextEditorTextSpan.Length;
+                            }
+                        }
+                    }
+
+                    outTextSpansList.Add(textSpan with
+                    {
+                        StartingIndexInclusive = startingIndexInclusive,
+                        EndingIndexExclusive = endingIndexExclusive
+                    });
+                }
+            }
+
+            return outTextSpansList;
+        }
+        catch (LuthetusTextEditorException)
+        {
+            return Array.Empty<TextEditorTextSpan>();
+        }
+    }
+
+    public (int FirstRowToSelectDataInclusive, int LastRowToSelectDataExclusive) PresentationGetBoundsInRowIndexUnits(
+    	TextEditorRenderBatch renderBatchLocal,
+    	TextEditorModel model,
+    	(int StartingIndexInclusive, int EndingIndexExclusive) boundsInPositionIndexUnits)
+    {
+    	try
+        {
+            var firstRowToSelectDataInclusive = renderBatchLocal.Model
+                .GetLineInformationFromPositionIndex(boundsInPositionIndexUnits.StartingIndexInclusive)
+                .Index;
+
+            var lastRowToSelectDataExclusive = renderBatchLocal.Model
+                .GetLineInformationFromPositionIndex(boundsInPositionIndexUnits.EndingIndexExclusive)
+                .Index +
+                1;
+
+            return (firstRowToSelectDataInclusive, lastRowToSelectDataExclusive);
+        }
+        catch (LuthetusTextEditorException)
+        {
+            return (0, 0);
+        }
+    }
+    
+    public string GetTextSelectionStyleCss(
+    	TextEditorRenderBatch renderBatchLocal,
+        int lowerPositionIndexInclusive,
+        int upperPositionIndexExclusive,
+        int rowIndex)
+    {
+    	try
+		{
+	        if (rowIndex >= renderBatchLocal.Model.LineEndList.Count)
+	            return string.Empty;
+	
+	        var line = renderBatchLocal.Model.GetLineInformation(rowIndex);
+	
+	        var selectionStartingColumnIndex = 0;
+	        var selectionEndingColumnIndex = line.EndPositionIndexExclusive - 1;
+	
+	        var fullWidthOfRowIsSelected = true;
+	
+	        if (lowerPositionIndexInclusive > line.StartPositionIndexInclusive)
+	        {
+	            selectionStartingColumnIndex = lowerPositionIndexInclusive - line.StartPositionIndexInclusive;
+	            fullWidthOfRowIsSelected = false;
+	        }
+	
+	        if (upperPositionIndexExclusive < line.EndPositionIndexExclusive)
+	        {
+	            selectionEndingColumnIndex = upperPositionIndexExclusive - line.StartPositionIndexInclusive;
+	            fullWidthOfRowIsSelected = false;
+	        }
+	
+	        var charMeasurements = renderBatchLocal.ViewModel.CharAndLineMeasurements;
+	
+	        var topInPixelsInvariantCulture = (rowIndex * charMeasurements.LineHeight).ToCssValue();
+	        var top = $"top: {topInPixelsInvariantCulture}px;";
+	
+	        var heightInPixelsInvariantCulture = charMeasurements.LineHeight.ToCssValue();
+	        var height = $"height: {heightInPixelsInvariantCulture}px;";
+	
+	        var selectionStartInPixels = selectionStartingColumnIndex * charMeasurements.CharacterWidth;
+	
+	        // selectionStartInPixels offset from Tab keys a width of many characters
+	        {
+	            var tabsOnSameRowBeforeCursor = renderBatchLocal.Model.GetTabCountOnSameLineBeforeCursor(
+	                rowIndex,
+	                selectionStartingColumnIndex);
+	
+	            // 1 of the character width is already accounted for
+	            var extraWidthPerTabKey = TextEditorModel.TAB_WIDTH - 1;
+	
+	            selectionStartInPixels += 
+	                extraWidthPerTabKey * tabsOnSameRowBeforeCursor * charMeasurements.CharacterWidth;
+	        }
+	
+	        var selectionStartInPixelsInvariantCulture = selectionStartInPixels.ToCssValue();
+	        var left = $"left: {selectionStartInPixelsInvariantCulture}px;";
+	
+	        var selectionWidthInPixels = 
+	            selectionEndingColumnIndex * charMeasurements.CharacterWidth - selectionStartInPixels;
+	
+	        // Tab keys a width of many characters
+	        {
+	            var lineInformation = renderBatchLocal.Model.GetLineInformation(rowIndex);
+	
+	            selectionEndingColumnIndex = Math.Min(
+	                selectionEndingColumnIndex,
+	                lineInformation.LastValidColumnIndex);
+	
+	            var tabsOnSameRowBeforeCursor = renderBatchLocal.Model.GetTabCountOnSameLineBeforeCursor(
+	                rowIndex,
+	                selectionEndingColumnIndex);
+	
+	            // 1 of the character width is already accounted for
+	            var extraWidthPerTabKey = TextEditorModel.TAB_WIDTH - 1;
+	
+	            selectionWidthInPixels += extraWidthPerTabKey * tabsOnSameRowBeforeCursor * charMeasurements.CharacterWidth;
+	        }
+	
+	        var widthCssStyleString = "width: ";
+	        var fullWidthValue = renderBatchLocal.ViewModel.ScrollbarDimensions.ScrollWidth;
+	
+	        if (renderBatchLocal.ViewModel.TextEditorDimensions.Width >
+	            renderBatchLocal.ViewModel.ScrollbarDimensions.ScrollWidth)
+	        {
+	            // If content does not fill the viewable width of the Text Editor User Interface
+	            fullWidthValue = renderBatchLocal.ViewModel.TextEditorDimensions.Width;
+	        }
+	
+	        var fullWidthValueInPixelsInvariantCulture = fullWidthValue.ToCssValue();
+	
+	        var selectionWidthInPixelsInvariantCulture = selectionWidthInPixels.ToCssValue();
+	
+	        if (fullWidthOfRowIsSelected)
+	            widthCssStyleString += $"{fullWidthValueInPixelsInvariantCulture}px;";
+	        else if (selectionStartingColumnIndex != 0 &&
+	                 upperPositionIndexExclusive > line.EndPositionIndexExclusive - 1)
+	            widthCssStyleString += $"calc({fullWidthValueInPixelsInvariantCulture}px - {selectionStartInPixelsInvariantCulture}px);";
+	        else
+	            widthCssStyleString += $"{selectionWidthInPixelsInvariantCulture}px;";
+	
+	        return $"{top} {height} {left} {widthCssStyleString}";
+		}
+		catch (LuthetusTextEditorException e)
+		{
+			Console.WriteLine(e);
+			return "display: none;";
+		}
+    }
+
+    public (int lowerRowIndexInclusive, int upperRowIndexExclusive) GetSelectionBoundsInRowIndexUnits(
+    	TextEditorRenderBatch renderBatchLocal,
+    	(int lowerPositionIndexInclusive, int upperPositionIndexExclusive) selectionBoundsInPositionIndexUnits)
+    {
+    	try
+        {
+            return TextEditorSelectionHelper.ConvertSelectionOfPositionIndexUnitsToRowIndexUnits(
+                renderBatchLocal.Model,
+                selectionBoundsInPositionIndexUnits);
+        }
+        catch (LuthetusTextEditorException)
+        {
+            return (0, 0);
+        }
+    }
+    
+    #endregion PresentationAndSelectionDriverClose
 
     public void Dispose()
     {
