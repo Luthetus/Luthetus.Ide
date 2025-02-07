b995b64d818d02b2868c6fc668ead30ddf5a0598
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-02-07 12:09:35 -0500

    SymbolDefinition -> record struct

--- Source/Lib/TextEditor/CompilerServices/Syntax/Symbols/SymbolDefinition.cs
+++ Source/Lib/TextEditor/CompilerServices/Syntax/Symbols/SymbolDefinition.cs
@@ -4,7 +4,7 @@ using Luthetus.TextEditor.RazorLib.CompilerServices.Interfaces;
 
 namespace Luthetus.TextEditor.RazorLib.CompilerServices.Syntax.Symbols;
 
-public sealed record SymbolDefinition
+public record struct SymbolDefinition
 {
     public SymbolDefinition(int scopeIndexKey, ISymbol symbol)
     {
@@ -19,5 +19,5 @@ public sealed record SymbolDefinition
     /// <summary>If a reference to a symbol is parsed prior to the definition, <see cref="IsFabricated"/> will be set to true. If later the definition is found, then this instance of <see cref="SymbolDefinition"/> will be overwritten to have this property with value of false.</summary>
     public bool IsFabricated { get; init; }
 
-    public ImmutableArray<SymbolReference> GetSymbolReferences() => SymbolReferences.ToImmutableArray();
+    public IReadOnlyList<SymbolReference> GetSymbolReferences() => SymbolReferences;
 }
