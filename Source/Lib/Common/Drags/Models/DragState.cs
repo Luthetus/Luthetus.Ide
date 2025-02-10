909dcf382d318d0aec73584b4c83395db96341d1
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-02-10 18:02:08 -0500

    Fix placement of DragState.cs

--- Source/Lib/Common/Drags/Models/DragState.cs
+++ Source/Lib/Common/Drags/Models/DragState.cs
@@ -2,9 +2,8 @@ using Fluxor;
 using Microsoft.AspNetCore.Components.Web;
 using Luthetus.Common.RazorLib.Dynamics.Models;
 
-namespace Luthetus.Common.RazorLib.Drags.Displays;
+namespace Luthetus.Common.RazorLib.Drags.Models;
 
-[FeatureState]
 public partial record DragState(
     bool ShouldDisplay,
     MouseEventArgs? MouseEventArgs,
