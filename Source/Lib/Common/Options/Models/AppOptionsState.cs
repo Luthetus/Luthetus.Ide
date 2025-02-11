3d40ba18a6d1881c82501926eaca3bf38ecb11d9
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-02-10 19:35:42 -0500

    AppOptionsState -> struct version

--- Source/Lib/Common/Options/Models/AppOptionsState.cs
+++ Source/Lib/Common/Options/Models/AppOptionsState.cs
@@ -4,7 +4,7 @@ using Luthetus.Common.RazorLib.Themes.Models;
 
 namespace Luthetus.Common.RazorLib.Options.Models;
 
-public record AppOptionsState(CommonOptions Options)
+public record struct AppOptionsState(CommonOptions Options)
 {
     public const int DEFAULT_FONT_SIZE_IN_PIXELS = 20;
     public const int MINIMUM_FONT_SIZE_IN_PIXELS = 5;
