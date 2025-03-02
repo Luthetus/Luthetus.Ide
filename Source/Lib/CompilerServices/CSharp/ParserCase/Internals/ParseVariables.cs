46a87d4a39e2f37ff21aa0e6538b35d02a853268
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-03-02 15:25:10 -0500

    Delete dead code in 'ParseVariables.cs'

--- Source/Lib/CompilerServices/CSharp/ParserCase/Internals/ParseVariables.cs
+++ Source/Lib/CompilerServices/CSharp/ParserCase/Internals/ParseVariables.cs
@@ -8,14 +8,7 @@ namespace Luthetus.CompilerServices.CSharp.ParserCase.Internals;
 
 public static class ParseVariables
 {
-    public static void HandleVariableReference(
-        SyntaxToken consumedIdentifierToken,
-        CSharpCompilationUnit compilationUnit,
-        ref CSharpParserModel parserModel)
-    {
-    }
-
-	/// <summary>Function invocation which uses the 'out' keyword.</summary>
+    /// <summary>Function invocation which uses the 'out' keyword.</summary>
     public static IVariableDeclarationNode? HandleVariableDeclarationExpression(
         TypeClauseNode consumedTypeClauseNode,
         SyntaxToken consumedIdentifierToken,
@@ -34,95 +27,5 @@ public static class ParseVariables
         compilationUnit.Binder.BindVariableDeclarationNode(variableDeclarationNode, compilationUnit);
         parserModel.CurrentCodeBlockBuilder.ChildList.Add(variableDeclarationNode);
         return variableDeclarationNode;
-    }
-    
-    /// <summary>
-    /// TODO: This method should return the 'VariableDeclarationNode?' just the same as <see cref="HandleVariableDeclarationExpression"/>
-    /// </summary>
-    public static void HandleVariableDeclarationStatement(
-        TypeClauseNode consumedTypeClauseNode,
-        SyntaxToken consumedIdentifierToken,
-        VariableKind variableKind,
-        CSharpCompilationUnit compilationUnit,
-        ref CSharpParserModel parserModel)
-    {
-    	var variableDeclarationNode = HandleVariableDeclarationExpression(
-			consumedTypeClauseNode,
-	        consumedIdentifierToken,
-	        variableKind,
-	        compilationUnit,
-	        ref parserModel);
-	        
-	    if (variableDeclarationNode is null)
-	    	return;
-	    	
-	    // if (variableKind == VariableKind.Local || variableKind == VariableKind.Closure)
-
-        if (parserModel.TokenWalker.Current.SyntaxKind == SyntaxKind.EqualsToken)
-        {
-            // Variable initialization occurs here.
-            HandleVariableAssignment(
-                consumedIdentifierToken,
-                parserModel.TokenWalker.Consume(),
-                compilationUnit,
-                ref parserModel);
-        }
-        else if (parserModel.TokenWalker.Current.SyntaxKind == SyntaxKind.EqualsCloseAngleBracketToken)
-        {
-        	HandlePropertyExpression(
-	            variableDeclarationNode,
-	            parserModel.TokenWalker.Consume(),
-	            parserModel.TokenWalker.Consume(),
-	            compilationUnit,
-	            ref parserModel);
-        }
-        else
-        {
-            if (variableDeclarationNode.TypeClauseNode.TypeIdentifierToken.SyntaxKind ==
-                SyntaxKind.VarTokenContextualKeyword)
-            {
-                /*compilationUnit.DiagnosticBag.ReportImplicitlyTypedVariablesMustBeInitialized(
-                    consumedIdentifierToken.TextSpan);*/
-            }
-        }
-
-        if (variableKind == VariableKind.Property &&
-            parserModel.TokenWalker.Current.SyntaxKind == SyntaxKind.OpenBraceToken)
-        {
-            HandlePropertyDeclaration(
-                variableDeclarationNode,
-                parserModel.TokenWalker.Consume(),
-                compilationUnit,
-                ref parserModel);
-        }
-        else
-        {
-            _ = parserModel.TokenWalker.Match(SyntaxKind.StatementDelimiterToken);
-        }
-    }
-
-    public static void HandlePropertyDeclaration(
-        IVariableDeclarationNode consumedVariableDeclarationNode,
-        SyntaxToken consumedOpenBraceToken,
-        CSharpCompilationUnit compilationUnit,
-        ref CSharpParserModel parserModel)
-    {
-    }
-
-    public static void HandlePropertyExpression(
-        IVariableDeclarationNode consumedVariableDeclarationNode,
-        SyntaxToken consumedEqualsToken,
-        SyntaxToken consumedCloseAngleBracketToken,
-        CSharpCompilationUnit compilationUnit,
-        ref CSharpParserModel parserModel)
-    {
-    }
-
-    public static void HandleVariableAssignment(
-        SyntaxToken consumedIdentifierToken,
-        SyntaxToken consumedEqualsToken,
-        CSharpCompilationUnit compilationUnit,
-        ref CSharpParserModel parserModel)
-    {
     }
 }
