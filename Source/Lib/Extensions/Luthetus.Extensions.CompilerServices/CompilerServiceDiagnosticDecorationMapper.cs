683cd5e76671b68eca6baef48852f3f203f4d9d6
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-03-05 11:49:57 -0500

    I'm in a using statement nightmare gonna commit this then open visual studio so I can refactor them

---
+++ Source/Lib/Extensions/Luthetus.Extensions.CompilerServices/CompilerServiceDiagnosticDecorationMapper.cs
@@ -0,0 +1,23 @@
+﻿using Luthetus.TextEditor.RazorLib.CompilerServices.Enums;
+using Luthetus.TextEditor.RazorLib.Decorations.Models;
+
+namespace Luthetus.TextEditor.RazorLib.CompilerServices;
+
+public class CompilerServiceDiagnosticDecorationMapper : IDecorationMapper
+{
+    public string Map(byte decorationByte)
+    {
+        var decoration = (CompilerServiceDiagnosticDecorationKind)decorationByte;
+
+        return decoration switch
+        {
+            CompilerServiceDiagnosticDecorationKind.None => string.Empty,
+            CompilerServiceDiagnosticDecorationKind.DiagnosticError => "luth_te_semantic-diagnostic-error",
+            CompilerServiceDiagnosticDecorationKind.DiagnosticHint => "luth_te_semantic-diagnostic-hint",
+            CompilerServiceDiagnosticDecorationKind.DiagnosticSuggestion => "luth_te_semantic-diagnostic-suggestion",
+            CompilerServiceDiagnosticDecorationKind.DiagnosticWarning => "luth_te_semantic-diagnostic-warning",
+            CompilerServiceDiagnosticDecorationKind.DiagnosticOther => "luth_te_semantic-diagnostic-other",
+            _ => string.Empty,
+        };
+    }
+}
