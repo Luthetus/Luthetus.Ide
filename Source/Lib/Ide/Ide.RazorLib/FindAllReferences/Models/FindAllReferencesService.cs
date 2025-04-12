313e52b2eb0a014c5df9bd30af56471bf104501a
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-04-11 20:22:50 -0400

    Interact with tree view

--- Source/Lib/Ide/Ide.RazorLib/FindAllReferences/Models/FindAllReferencesService.cs
+++ Source/Lib/Ide/Ide.RazorLib/FindAllReferences/Models/FindAllReferencesService.cs
@@ -1,9 +1,17 @@
+using Luthetus.Common.RazorLib.TreeViews.Models;
 using Luthetus.Extensions.CompilerServices.Syntax.Nodes;
 
 namespace Luthetus.Ide.RazorLib.FindAllReferences.Models;
 
 public class FindAllReferencesService : IFindAllReferencesService
 {
+	private readonly ITreeViewService _treeViewService;
+
+	public FindAllReferencesService(ITreeViewService treeViewService)
+	{
+		_treeViewService = treeViewService;
+	}
+
 	private readonly object _stateModificationLock = new();
 
 	private FindAllReferencesState _findAllReferencesState = new();
@@ -24,6 +32,41 @@ public class FindAllReferencesService : IFindAllReferencesService
             	SyntaxName = syntaxName,
             	TypeDefinitionNode = typeDefinitionNode,
             };
+            
+            // The following is way too much code for a lock that might've
+            // been entered by the UI thread.
+            
+            var treeViewNodeList = typeDefinitionNode.ReferenceHashSet.Select(x =>
+	    		(TreeViewNoType)new TreeViewFindAllReferences(
+	    			x,
+					false,
+					false))
+				.ToArray();
+				
+			var adhocRoot = TreeViewAdhoc.ConstructTreeViewAdhoc(treeViewNodeList);
+	        var firstNode = treeViewNodeList.FirstOrDefault();
+	
+	        var activeNodes = firstNode is null
+	            ? new List<TreeViewNoType>()
+	            : new() { firstNode };
+	
+	        if (!_treeViewService.TryGetTreeViewContainer(FindAllReferencesState.TreeViewContainerKey, out _))
+	        {
+	            _treeViewService.ReduceRegisterContainerAction(new TreeViewContainer(
+	                FindAllReferencesState.TreeViewContainerKey,
+	                adhocRoot,
+	                activeNodes));
+	        }
+	        else
+	        {
+	            _treeViewService.ReduceWithRootNodeAction(FindAllReferencesState.TreeViewContainerKey, adhocRoot);
+	
+	            _treeViewService.ReduceSetActiveNodeAction(
+	                FindAllReferencesState.TreeViewContainerKey,
+	                firstNode,
+	                true,
+	                false);
+	        }
         }
 
         FindAllReferencesStateChanged?.Invoke();
