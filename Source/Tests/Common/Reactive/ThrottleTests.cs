e19452a348973189604dbf2b38f0e72763bd6adb
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-05-12 17:07:22 -0400

    "kill" unit test button

--- Source/Tests/Common/Reactive/ThrottleTests.cs
+++ Source/Tests/Common/Reactive/ThrottleTests.cs
@@ -3,9 +3,21 @@ namespace Luthetus.Common.Tests.Reactive;
 public class ThrottleTests
 {
 	[Fact]
-	public void Aaa()
+	public async Task Aaa()
 	{
+		var count = 0;
 		
+		var countTask = Task.Run(async () =>
+		{
+			while (true)
+			{
+				count++;
+				Console.WriteLine(count);
+				await Task.Delay(100);
+			}
+		});
+		
+		await countTask;
 	}
 }
 
