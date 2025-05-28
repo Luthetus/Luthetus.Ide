759fa2dc8d92445e45cb30a56daa5eb3524879d9
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-05-28 15:42:42 -0400

    Remove reference to git C# project

--- Source/Lib/Extensions/Extensions.Config/Installations/Models/ServiceCollectionExtensions.cs
+++ Source/Lib/Extensions/Extensions.Config/Installations/Models/ServiceCollectionExtensions.cs
@@ -5,7 +5,7 @@ using Luthetus.Ide.RazorLib.Installations.Models;
 using Luthetus.Extensions.DotNet.Installations.Models;
 using Luthetus.Extensions.Config.CompilerServices;
 using Luthetus.Extensions.Config.Decorations;
-using Luthetus.Extensions.Git.Installations.Models;
+// using Luthetus.Extensions.Git.Installations.Models;
 using Luthetus.TextEditor.RazorLib.CompilerServices;
 
 namespace Luthetus.Extensions.Config.Installations.Models;
@@ -19,7 +19,7 @@ public static class ServiceCollectionExtensions
     {
         return services
             .AddLuthetusExtensionsDotNetServices(hostingInformation, configure)
-            .AddLuthetusExtensionsGitServices(hostingInformation, configure)
+            // .AddLuthetusExtensionsGitServices(hostingInformation, configure)
             .AddScoped<ICompilerServiceRegistry, ConfigCompilerServiceRegistry>()
             .AddScoped<IDecorationMapperRegistry, DecorationMapperRegistry>();
     }
